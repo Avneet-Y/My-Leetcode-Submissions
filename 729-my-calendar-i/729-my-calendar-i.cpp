9d9f6e65d2fe7ad32980a08f8072a0686dd555ad
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-03 16:14:07 +0530

    Time: 657 ms (5.03%), Space: 43.8 MB (8.21%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,31 @@
+class MyCalendar {
+    
+   private: map<int,int> m;
+public:
+    
+    bool book(int start, int end) {
+        m[start]++;
+        m[end]--;
+        int sum = 0;
+        for(auto itr = m.begin(); itr!=m.end(); itr++)
+        {
+            sum += itr->second; 
+            
+            //before closing if any event other start in time range then, we could give -1 and +1 for closing, and gave extra +1 i.e intersection ..eg 10 to 20  10 is -1 and 20 is +1 (+1-1=0), 15 to 25, 15 is -1 ,25 is +1 now 15 is coming so sum is 1+1 that is 2 so false
+            
+            if(sum>1)
+            {
+                m[start]--;
+                m[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
