7a11345690d8645946092aeaa1f35ead152c4b75
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-04 20:22:12 +0530

    Time: 3 ms (47.73%), Space: 6 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,26 +1,56 @@
+// class Solution {
+// public:
+ 
+//     double myPow(double x, int n)
+//     {
+//         double ans = 1;
+//         if(n==0)
+//             return 1;
+//         else if(n < 0)
+//         {
+//             n = abs(n);
+//             x = (1/x);
+//         }
+        
+//         if(n%2 == 0)
+//         {
+//             return myPow(x*x, n/2);
+        
+//         }
+//         else
+//         {
+//             return x*myPow(x*x, n/2);
+//         }
+//     }
+    
+// };
+
+
 class Solution {
 public:
-   
+ 
     double myPow(double x, int n)
     {
         double ans = 1;
-        if(n==0)
-            return 1;
+        if (n == INT_MAX) 
+            return x;
+        else if (n == INT_MIN)
+        { 
+             if(x == 1 || x == -1) 
+                 return 1; 
+            else 
+                return 0;
+        }
         else if(n < 0)
         {
             n = abs(n);
             x = (1/x);
         }
         
-        if(n%2 == 0)
-        {
-            return myPow(x*x, n/2);
-        
-        }
-        else
-        {
-            return x*myPow(x*x, n/2);
-        }
-    }
+    for(int i=0;i<n;i++)
+        ans = ans*x;
     
+        return ans;
+    }
 };
+        
