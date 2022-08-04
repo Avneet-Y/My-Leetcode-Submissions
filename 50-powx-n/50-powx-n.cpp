88247f6a927ca8fd12322faca7a3d6da24a856d2
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-04 20:02:52 +0530

    Time: 3 ms (47.73%), Space: 6 MB (43.77%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   
+    double myPow(double x, int n)
+    {
+        double ans = 1;
+        if(n==0)
+            return 1;
+        else if(n < 0)
+        {
+            n = abs(n);
+            x = (1/x);
+        }
+        
+        if(n%2 == 0)
+        {
+            return myPow(x*x, n/2);
+        
+        }
+        else
+        {
+            return x*myPow(x*x, n/2);
+        }
+    }
+    
+};
