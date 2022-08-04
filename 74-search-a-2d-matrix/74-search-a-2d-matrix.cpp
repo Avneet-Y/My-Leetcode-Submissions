a7ffe3f99ca10b4edd1ad5228be08c2ff8fc22ef
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-04 16:09:23 +0530

    Time: 14 ms (7.56%), Space: 9.6 MB (51.76%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,15 +1,36 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m=matrix.size();
+//         int n=matrix[0].size();
+//         for(int i=0; i<m; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(matrix[i][j]==target)
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0; i<m; i++)
+        int i=0, j=n-1;
+        while(i<m && j>=0)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(matrix[i][j]==target)
-                    return true;
-            }
+            int present = matrix[i][j];
+            if(present == target)
+                return true;
+            if(target > present)
+                i++;
+            else
+                j--;
         }
         return false;
     }
