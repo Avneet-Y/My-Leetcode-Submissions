875c0179383f12e14fdae2688e77b5c7081864ce
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-06 11:07:56 +0530

    Time: 3 ms (40.36%), Space: 6 MB (80.73%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        int ans;
+        ans = ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+             return ans;
+        
+    }
+};
