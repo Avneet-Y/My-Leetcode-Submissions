fc2ff187c3bf3675500f9db7862a1a53c3b97a72
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-07 13:32:01 +0530

    Time: 6 ms (23.54%), Space: 6 MB (25.14%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int ans;
-        if(num < 10)
-            return num;
-        ans = addDigits(num/10) + (num%10);
-        if(ans<10)
-            return ans;
-        return addDigits(ans/10) + (ans%10);
+        // int ans;
+        // if(num < 10)
+        //     return num;
+        // ans = addDigits(num/10) + (num%10);
+        // if(ans<10)
+        //     return ans;
+        // return addDigits(ans/10) + (ans%10);
+         if(num == 0)
+            return 0; 
+         else
+             return(1 + (num - 1) % 9);
     }
 };
