2a8c0cd9be485323b8d42b2f33676a8cf8e81feb
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-09 16:04:41 +0530

    Time: 24 ms (13.20%), Space: 11.2 MB (88.06%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,40 +1,66 @@
+// class Solution {
+// public:
+//    static bool comp(pair<int,int>&a, pair<int,int>&b)
+//    {     // freq,ele ->pair
+//       if(a.first == b.first)  //having same freq
+//           return a.second > b.second;
+     
+//        return a.first < b.first;   //return having more freq
+//    }
+    
+//     vector<int> frequencySort(vector<int> &nums) {
+    
+//         unordered_map<int,int>mp;    
+//         for(auto i : nums)
+//             mp[i]++;
+
+//         vector<pair<int,int>> v;
+    
+//         for(auto i : mp)
+//         {
+//             v.push_back({i.second,i.first}); // frq , element
+//         }
+    
+//         sort(v.begin(),v.end(),comp);
+    
+//         vector<int> ans;
+    
+//         for(int i=0; i<v.size(); i++)
+//         {
+//             while(v[i].first--)
+//             {
+//                 ans.push_back(v[i].second);
+//             }
+//         }
+//      return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-   static bool comp(pair<int,int>&a, pair<int,int>&b)
-   {     // freq,ele ->pair
-      if(a.first == b.first)  //having same freq
-          return a.second > b.second;
-     
-       return a.first < b.first;   //return having more freq
-   }
     
     vector<int> frequencySort(vector<int> &nums) {
     
         unordered_map<int,int>mp;    
         for(auto i : nums)
             mp[i]++;
+        
+        sort(nums.begin(), nums.end(), [&](int a, int b)
+             {
+                return mp[a] != mp[b] ? mp[a] < mp[b] : a > b; });
 
-        vector<pair<int,int>>v;
-    
-        for(auto i : mp)
-        {
-            v.push_back({i.second,i.first}); // frq , element
-        }
-    
-        sort(v.begin(),v.end(),comp);
-    
-        vector<int> ans;
-    
-        for(int i=0; i<v.size(); i++)
-        {
-            while(v[i].first--)
-            {
-                ans.push_back(v[i].second);
-            }
-        }
-     return ans;
+       
+     return nums;
     }
 };
+
+
+
+
+
 
 
 
