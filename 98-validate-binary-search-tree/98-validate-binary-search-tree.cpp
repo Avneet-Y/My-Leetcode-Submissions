b88dfcbc8c48cd9d7ced3b8df7e11b2cd0b1bcc8
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-11 20:23:52 +0530

    Time: 17 ms (65.02%), Space: 21.8 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,18 +12,34 @@
 class Solution {
 public:
    
-    bool isValidBST(TreeNode* root) {
-    return isValidBST(root, NULL, NULL);
-}
+//     bool isValidBST(TreeNode* root) {
+//     return isValidBST(root, NULL, NULL);
+// }
 
-bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
-    if(root==NULL) 
-        return true;
-    if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
-        return false;
-    return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
-        
+// bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+//     if(root==NULL) 
+//         return true;
+//     if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+//         return false;
+//     return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
     
-     
+        vector<int> inord; // to store inorder order traversal
+        void solve(TreeNode* root) 
+        {
+            if(root == NULL) 
+                return; 
+            solve(root->left);
+            inord.push_back(root->val); 
+            solve(root->right); 
+        }
+    
+    bool isValidBST(TreeNode* root) {   
+        solve(root);   //  in-order manner
+        for(int i=1; i<inord.size(); i++) {
+            if(inord[i] <= inord[i-1])   //  BST condition false
+                return false; 
+        }
+        return true;    
+    
     }
 };
