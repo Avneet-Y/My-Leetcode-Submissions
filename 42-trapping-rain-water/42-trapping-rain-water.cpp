0b3cd56776097253b9cc01a191439ec7fca456c7
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-16 22:34:50 +0530

    Time: 15 ms (92.16%), Space: 19.8 MB (82.05%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,24 +5,48 @@ public:
         int ans = 0;
         int n = height.size();
         
-        int lmax[n], rmax[n];
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
+//         int lmax[n], rmax[n];
+//         lmax[0] = height[0];
+//         rmax[n-1] = height[n-1];
         
-        for(int i = 1; i<n ;i++)
-        {
-            lmax[i] = max(height[i], lmax[i-1]);
-        }
+//         for(int i = 1; i<n ;i++)
+//         {
+//             lmax[i] = max(height[i], lmax[i-1]);
+//         }
         
-        for(int i = n-2; i>=0 ;i--)
-        {
-            rmax[i] = max(height[i], rmax[i+1]);
-        }
+//         for(int i = n-2; i>=0 ;i--)
+//         {
+//             rmax[i] = max(height[i], rmax[i+1]);
+//         }
         
-        for(int i=0; i<n; i++)
-            ans = ans + (min(lmax[i], rmax[i]) - height[i]);
+//         for(int i=0; i<n; i++)
+//             ans = ans + (min(lmax[i], rmax[i]) - height[i]);
             
-        return ans;
+//         return ans;
         
+        int i=0;
+        int j = n-1;
+        
+        int lmax=0,rmax=0;
+        while(i <= j)
+        {
+            if(height[i] <= height[j])
+            {
+                if(height[i] >= lmax)
+                    lmax = height[i];   
+                else
+                    ans += lmax - height[i];
+                i++;
+            } 
+            else
+            {
+                if(height[j] >= rmax)
+                    rmax = height[j];
+                else
+                    ans += rmax - height[j];
+                j--;
+            } 
+        }
+        return ans;
     }
 };
