085a6cb8d64bb45868fb69b72c052b1b37fa9363
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-20 22:35:50 +0530

    Time: 36 ms (38.28%), Space: 17.9 MB (64.62%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        long sum = (n*(n+1))/2;
-        for(int i=0; i<n ;i++)
+        // long sum = (n*(n+1))/2;
+        // for(int i=0; i<n ;i++)
+        // {
+        //     sum -= nums[i];
+        // }
+        // return sum;
+        
+        int xorele = n;    
+        for(int i=0; i<n; i++)
         {
-            sum -= nums[i];
+            xorele = xorele ^ i;
+            xorele = xorele ^ nums[i];
         }
-        return sum;  
+            return xorele;
+        
+        
     }
 };
