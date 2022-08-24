99267152b4985533ae784a745217dbc571e0d3ea
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-25 01:23:13 +0530

    Time: 3 ms (59.19%), Space: 6 MB (47.96%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,13 +1,35 @@
+// class Solution {
+// public:
+//     int hammingWeight(uint32_t n) {
+//         int count = 0;
+//         while(n>0)
+//         {
+//             n &= (n - 1);
+//             count++;
+//         }
+//         return count;
+//          int hammingWeight(uint32_t n) {
+//         return __builtin_popcount(n);
+//     }
+// };
+
+// inbuilt function bitset.
+
+// class Solution {
+// public:
+//     int hammingWeight(uint32_t n) {
+         
+//         return __builtin_popcount(n);
+//     }
+// };
+    
+
+
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count = 0;
-        while(n>0)
-        {
-            n &= (n - 1);
-            count++;
-        }
-        return count;
         
+        return bitset<32>(n).count();
     }
+        
 };
