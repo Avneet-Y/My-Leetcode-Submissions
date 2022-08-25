e518a2bda693fc251c47a44ffabffba620a985bf
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-25 14:50:19 +0530

    Time: 22 ms (62.95%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
        
-        int count[26] = {0};
+//         int count[26] = {0};
         
-        for(auto i: magazine)
-            count[i - 'a']++;
+//         for(auto i: magazine)
+//             count[i - 'a']++;
         
-        for(auto i: ransomNote)
+//         for(auto i: ransomNote)
+//         {
+//             if(count[i - 'a']-- <= 0)
+//                 return false;
+//         }
+//         return true;
+        unordered_map<char , int >m;
+        int count=0;
+        for(int i=0 ; i<magazine.length() ; i++)
         {
-            if(count[i - 'a']-- <= 0)
+            m[magazine[i]]++;
+        }
+        for(int i=0 ; i<ransomNote.size() ; i++)
+        { 
+            if(m[ransomNote[i]]==0) 
                 return false;
+            m[ransomNote[i]]--;
         }
         return true;
         
