e165ce07273d365a337b96bad43f25bd2e7b9c5a
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-08-25 22:11:45 +0530

    Time: 320 ms (63.34%), Space: 84.2 MB (92.13%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,8 +1,71 @@
+// class StockSpanner {
+// public:
+//      stack<int> st;
+//      vector<int> vec;
+//      int i=0;
+    
+//     StockSpanner() {
+        
+//     }
+    
+//     int next(int price) 
+//     {
+//         vec.push_back(price);
+//         int span;
+        
+//             while(st.empty()==false && vec[st.top()] <= price)
+//                 st.pop();
+//             if(st.empty())
+//                 span = i+1;
+//             else
+//                 span = i - st.top(); 
+            
+//             st.push(i++);
+        
+//         return span;
+        
+//     }
+// };
+
+// /**
+//  * Your StockSpanner object will be instantiated and called as such:
+//  * StockSpanner* obj = new StockSpanner();
+//  * int param_1 = obj->next(price);
+//  */
+
+
+
+// class StockSpanner {
+// public:
+//      stack<int> st;
+    
+//     StockSpanner() {
+        
+//     }
+    
+//     int next(int price) 
+//     {
+//         int span;
+        
+//             while(st.empty()==false && vec[st.top()] <= price)
+//                 st.pop();
+//             if(st.empty())
+//                 span = i+1;
+//             else
+//                 span = i - st.top(); 
+            
+//             st.push(i++);
+        
+//         return span;
+        
+//     }
+// };
+
+
+
 class StockSpanner {
 public:
-     stack<int> st;
-     vector<int> vec;
-     int i=0;
+     stack<pair<int, int>> st;
     
     StockSpanner() {
         
@@ -10,22 +73,20 @@ public:
     
     int next(int price) 
     {
-        vec.push_back(price);
-        int span;
-        
-            while(st.empty()==false && vec[st.top()] <= price)
-                st.pop();
-            if(st.empty())
-                span = i+1;
-            else
-                span = i - st.top(); 
-            
-            st.push(i++);
-        
+        int span = 1;
+        while(st.size()>0 && st.top().first <= price)
+        {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
         return span;
-        
+    
     }
 };
+
+
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
