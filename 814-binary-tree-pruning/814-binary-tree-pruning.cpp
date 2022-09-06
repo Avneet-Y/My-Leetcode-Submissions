1cb3146846b9640aa57fad6f5de09e526510836f
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-06 20:29:58 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -9,40 +9,60 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int helper(TreeNode* root)
+//     {
+//         if(root==NULL)
+//             return 0;
+        
+//         if(root->left == NULL && root->right == NULL)
+//         {
+//             if(root->val == 0)
+//                 return 0;
+//             else
+//                  return 1;
+//         }
+        
+//         int leftsubt = helper(root->left);
+//         int rightsubt = helper(root->right);
+        
+//         if(leftsubt == 0)
+//             root->left = NULL;
+        
+//         if(rightsubt == 0)
+//             root->right = NULL;
+        
+//         return leftsubt + rightsubt + (root->val == 1 ? 1 : 0);
+            
+//     }
+    
+//     TreeNode* pruneTree(TreeNode* root) {
+//         //subtree me se 1 vali nodes remove krni hai
+        
+//         if(helper(root) == 0)
+//             return NULL;
+        
+//         return root;
+    
+    
+    
+    
+//     }
+// };
+
+
 class Solution {
 public:
-    int helper(TreeNode* root)
-    {
-        if(root==NULL)
-            return 0;
-        
-        if(root->left == NULL && root->right == NULL)
-        {
-            if(root->val == 0)
-                return 0;
-            else
-                 return 1;
-        }
-        
-        int leftsubt = helper(root->left);
-        int rightsubt = helper(root->right);
-        
-        if(leftsubt == 0)
-            root->left = NULL;
-        
-        if(rightsubt == 0)
-            root->right = NULL;
-        
-        return leftsubt + rightsubt + (root->val == 1 ? 1 : 0);
-            
-    }
-    
+         
     TreeNode* pruneTree(TreeNode* root) {
-        //subtree me se 1 vali nodes remove krni hai
         
-        if(helper(root) == 0)
+        if (root == NULL) 
             return NULL;
-        
-        return root;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->val == 1 || root->left != NULL || root->right != NULL) 
+            return root; //current subtree contain 1 or not?
+        return NULL;     
     }
 };
