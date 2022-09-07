e58b3098ec6a271d52eeeb646ebdcb7850677710
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-08 00:22:19 +0530

    Time: 276 ms (24.04%), Space: 50.6 MB (25.42%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -4,19 +4,33 @@ public:
         // sort(nums.begin(), nums.end());
         // return nums;
         
-        priority_queue<int> pq;
+//         priority_queue<int> pq;
+//         vector<int> ans;
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             pq.push(nums[i]);
+//         }
+        
+//         while(pq.size()!=0)
+//         {
+//             ans.push_back(pq.top());
+//             pq.pop();
+//         }
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+        
+        priority_queue<int, vector<int>, greater<int>> minheap;
         vector<int> ans;
         for(int i=0; i<nums.size(); i++)
         {
-            pq.push(nums[i]);
+            minheap.push(nums[i]);
         }
         
-        while(pq.size()!=0)
+        while(minheap.size()!=0)
         {
-            ans.push_back(pq.top());
-            pq.pop();
+            ans.push_back(minheap.top());
+            minheap.pop();
         }
-        reverse(ans.begin(), ans.end());
         return ans;
         
     }
