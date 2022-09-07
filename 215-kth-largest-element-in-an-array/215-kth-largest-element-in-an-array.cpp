193290095a9ddc13125838b1b358ea49e0048e1f
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-07 20:30:11 +0530

    Time: 112 ms (84.78%), Space: 47.3 MB (29.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        // Using Minheap prioriy queue
         
-        priority_queue<int, vector<int>, greater<int>> minheap;
-        for(int i=0; i<nums.size(); i++)
-        {
-            minheap.push(nums[i]);
-            if(minheap.size() > k)
-            {
-                minheap.pop();
-            }
-        }
-        return minheap.top();
+        // priority_queue<int, vector<int>, greater<int>> minheap;
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     minheap.push(nums[i]);
+        //     if(minheap.size() > k)
+        //     {
+        //         minheap.pop();
+        //     }
+        // }
+        // return minheap.top();
+        
+        // Using Maxheap 
+        priority_queue<int> maxheap(nums.begin(),nums.end());
+	    for(int i=1; i<k; i++) 
+            maxheap.pop();
+	    
+        return maxheap.top();
         
     }
 };
