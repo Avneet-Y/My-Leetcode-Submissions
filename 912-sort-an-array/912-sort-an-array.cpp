f5edd78adde871c6493a3ade842c2475284fcd91
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-08 00:19:57 +0530

    Time: 334 ms (19.41%), Space: 50.6 MB (25.56%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+        // sort(nums.begin(), nums.end());
+        // return nums;
+        
+        priority_queue<int> pq;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++)
+        {
+            pq.push(nums[i]);
+        }
+        
+        while(pq.size()!=0)
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
     }
 };
