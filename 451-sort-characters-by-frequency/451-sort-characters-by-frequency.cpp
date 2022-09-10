1bb6e70fc10f75847e9a552a4607f197dcea9c49
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-10 23:12:54 +0530

    Time: 517 ms (5.02%), Space: 8 MB (89.88%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,28 +2,42 @@ class Solution {
 public:
     string frequencySort(string s) {
        
-        unordered_map<char,int> mp;
-        string ans;
-        for(int i=0; i<s.size() ; i++)
+//         unordered_map<char,int> mp;
+//         string ans;
+//         for(int i=0; i<s.size() ; i++)
+//         {
+//             mp[s[i]]++;
+//         }
+        
+//         priority_queue<pair<int, char>> maxh;
+//         for(auto it : mp)
+//         {
+//             maxh.push({it.second, it.first});
+//         }
+//         while(maxh.size() !=0 )
+//         {
+//             int freq = maxh.top().first;
+//             int ele = maxh.top().second;
+//             for(int i=1; i<=freq; i++)
+//             {
+//                 ans.push_back(ele);
+//             }
+//             maxh.pop();
+//         }
+//         return ans;
+       
+        unordered_map<char,int>mp;    
+        for(auto i :s)
         {
-            mp[s[i]]++;
+            mp[i]++;
         }
         
-        priority_queue<pair<int, char>> maxh;
-        for(auto it : mp)
-        {
-            maxh.push({it.second, it.first});
-        }
-        while(maxh.size() !=0 )
-        {
-            int freq = maxh.top().first;
-            int ele = maxh.top().second;
-            for(int i=1; i<=freq; i++)
-            {
-                ans.push_back(ele);
-            }
-            maxh.pop();
-        }
-        return ans;
+        sort(s.begin(), s.end(), [&mp](char a, char b)
+             {
+                return mp[a] > mp[b] || (mp[a] == mp[b] && a > b); });
+
+       
+     return s;
+       
      }
 };
