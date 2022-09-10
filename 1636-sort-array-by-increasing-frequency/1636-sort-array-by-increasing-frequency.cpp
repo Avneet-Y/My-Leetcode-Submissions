5c317cd9e13896445a467cedc3dd6331a1febffc
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-10 22:43:41 +0530

    Time: 11 ms (71.50%), Space: 11.2 MB (74.14%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -39,6 +39,7 @@
 
 
 
+/*
 class Solution {
 public:
     
@@ -54,9 +55,43 @@ public:
 
        
      return nums;
+       
     }
 };
+*/
 
+
+class Solution {
+public:
+    
+    vector<int> frequencySort(vector<int> &nums) {
+
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int i=0; i<nums.size() ; i++)
+        {
+            mp[nums[i]]++;
+        }
+        
+        priority_queue<pair<int, int>> maxh;
+        for(auto it : mp)
+        {
+            maxh.push({it.second, -1*(it.first)});
+        }
+        while(maxh.size() !=0 )
+        {
+            int freq = maxh.top().first;
+            int ele = maxh.top().second;
+            for(int i=1; i<=freq; i++)
+            {
+                ans.push_back(-1*(ele));
+            }
+            maxh.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
 
 
 
