a5882631a87cbf0d4496e77fd60eed37fa0e5bd0
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-09-20 00:53:45 +0530

    Time: 9 ms (22.66%), Space: 6.8 MB (27.01%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> um;
+//         unordered_map<char, int> um;
 		
-        for(int i = 0; i < t.size(); i++)
-        {
-			um[t[i]]++;
-		}
+//         for(int i = 0; i < t.size(); i++)
+//         {
+// 			um[t[i]]++;
+// 		}
+// 		for(int i = 0; i < s.size(); i++)
+//         {
+// 			um[s[i]]--;
+// 		}
+// 		for(auto it = um.begin(); it != um.end(); it++)
+//         {
+// 			if(it->second == 1)
+//             {
+// 				return it->first;
+// 			}
+// 		}
+// 		return 'a'; 
+       
+        char ans = t[s.size()];
 		for(int i = 0; i < s.size(); i++)
         {
-			um[s[i]]--;
+			ans ^= s[i] ^ t[i];
 		}
-		for(auto it = um.begin(); it != um.end(); it++)
-        {
-			if(it->second == 1)
-            {
-				return it->first;
-			}
-		}
-		return 'a'; 
-       
+		return ans;
         
     }
 };
