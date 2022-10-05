c830222cfd0b78bf3ee3f6ab1a78924b7c98a489
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-10-06 01:21:23 +0530

    Time: 38 ms (91.19%), Space: 23.3 MB (47.45%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<string> ans;
-        unordered_map<int,string> mp;
+//         vector<string> ans;
+//         unordered_map<int,string> mp;
     
-        for(int i=0; i<names.size(); i++)
-        {
-            mp[heights[i]] = names[i];
-        }
+//         for(int i=0; i<names.size(); i++)
+//         {
+//             mp[heights[i]] = names[i];
+//         }
     
-        sort(heights.rbegin(),heights.rend());
+//         sort(heights.rbegin(),heights.rend());
     
-        for(int i=0; i<heights.size(); i++)
-        {
-            ans.push_back(mp[heights[i]]);
-        }
+//         for(int i=0; i<heights.size(); i++)
+//         {
+//             ans.push_back(mp[heights[i]]);
+//         }
     
-        return ans;
+//         return ans;
         
+        priority_queue<pair<int, string>> pq;
+        
+        for(int i = 0; i < heights.size(); i++)
+        {
+            pq.push({heights[i], names[i]});
+        }
+        
+        vector<string> ans;
+        
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
         
     }
 };
