8f4bf9567a213829b32d88023eb216f815d37014
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-10-19 01:56:52 +0530

    Time: 11 ms (96.60%), Space: 10.6 MB (86.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
      int dp[1001][1001];
@@ -17,5 +19,38 @@ public:
         int n = s1.size();
         int m = s2.size();
         return lcs(s1, s2, n, m);
+    }   
+};
+*/
+
+
+class Solution {
+public:
+  int lcsdp(string X, string Y, int n, int m){
+    int dp[n+1][m+1];
+    for(int i=0; i<n+1; i++)
+    {
+        for(int j=0; j<m+1; j++)
+        {
+            if(i == 0 || j == 0) 
+                dp[i][j] = 0;
+        }
+    }
+    for (int i = 1; i < n+1; i++)
+    {
+        for (int j = 1; j < m+1; j++)
+        {
+            if(X[i-1] == Y[j-1]) 
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else 
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        }
+    }
+    return dp[n][m];
+}
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        return lcsdp(s1, s2, n, m);
     }
 };
