6bb90a7389cff1adaa502f1f96c639e799fcfb74
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-10-23 13:43:33 +0530

    Time: 185 ms (9.97%), Space: 33 MB (13.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,21 +2,46 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int> ans;
-        map<int, int> mp;
-        for(auto i : nums)
-            mp[i]++;
-        for(auto it = mp.begin(); it!= mp.end(); it++)
+        // map<int, int> mp;
+        // for(auto i : nums)
+        //     mp[i]++;
+        // for(auto it = mp.begin(); it!= mp.end(); it++)
+        // {
+        //     if(it->second > 1)
+        //         ans.push_back(it->first);
+        // }
+        // for(int i = 1; i <= nums.size(); i++)
+        // {
+        //     if(mp.find(i) == mp.end())
+        //     {
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;  
+        
+        // Using xor
+        
+        int n = nums.size();
+        set<int> s(nums.begin(), nums.end());
+        int xorele = 0;
+        for (auto i : s)
         {
-            if(it->second > 1)
-                ans.push_back(it->first);
+            xorele ^= i;
         }
-        for(int i = 1; i <= nums.size(); i++)
+        int repeat = xorele;
+		
+        for (int i = 0; i < n; i++)
         {
-            if(mp.find(i) == mp.end())
-            {
-                ans.push_back(i);
-            }
+            repeat ^= nums[i];
         }
-        return ans;            
+        
+		for (int i = 1; i <= n; i++)
+        {
+            xorele ^= i;
+        }
+        int missing = xorele;
+        ans.push_back(repeat);
+        ans.push_back(missing);
+        return ans;
     }
 };
