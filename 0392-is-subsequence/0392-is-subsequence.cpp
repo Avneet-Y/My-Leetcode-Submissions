26c8433856692e87ba4e0a7bc38d691d85f8bad0
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-10-27 00:06:15 +0530

    Time: 5 ms (27.45%), Space: 6.8 MB (11.24%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -47,43 +47,73 @@ public:
 
 
 //Using LCS {dp}
+// class Solution {
+// public:
+//     int lcs(string s1, string s2, int m, int n)
+//     {
+//         int dp[m+1][n+1];
+//         for(int i = 0; i < m+1; i++)
+//         {
+//             for(int j = 0; j < n+1; j++)
+//             {
+//                 if(i == 0 || j == 0)
+//                     dp[i][j] = 0;
+//             }
+//         }
+//         for(int i = 1; i < m+1; i++)
+//         {
+//             for(int j = 1; j < n+1; j++)
+//             {
+//                 if(s1[i-1] == s2[j-1])
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 else
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//             }
+//         }
+//         return dp[m][n];
+//     }
+    
+    
+//      bool isSubsequence(string s, string t) {
+//          int m = s.length();
+//          int n = t.length();
+//          int len = lcs(s, t, m, n);
+//          if(m == 0)
+//              return true;
+//          if(n == 0)
+//              return false;
+//          if(len == min(m, n))
+//              return true;
+//          return false;
+//      }
+// };
+
+
+
 class Solution {
 public:
-    int lcs(string s1, string s2, int m, int n)
-    {
-        int dp[m+1][n+1];
-        for(int i = 0; i < m+1; i++)
-        {
+     bool isSubsequence(string s, string t) {
+         int m = s.length();
+         int n = t.length();
+         int dp[m+1][n+1];
+         for(int i = 0; i < m+1; i++)
+         {
             for(int j = 0; j < n+1; j++)
             {
                 if(i == 0 || j == 0)
                     dp[i][j] = 0;
-            }
-        }
-        for(int i = 1; i < m+1; i++)
-        {
+             }
+         }
+         for(int i = 1; i < m+1; i++)
+         {
             for(int j = 1; j < n+1; j++)
             {
-                if(s1[i-1] == s2[j-1])
+                if(s[i-1] == t[j-1])
                     dp[i][j] = 1 + dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[m][n];
-    }
-    
-    
-     bool isSubsequence(string s, string t) {
-         int m = s.length();
-         int n = t.length();
-         int len = lcs(s, t, m, n);
-         if(m == 0)
-             return true;
-         if(n == 0)
-             return false;
-         if(len == min(m, n))
-             return true;
-         return false;
+             }
+         }
+        return dp[m][n] == m;
      }
 };
