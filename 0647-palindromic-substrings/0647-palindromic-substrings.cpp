4bc4116648559d7791991df6ae84802f3d1dc7d7
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-11-02 01:40:11 +0530

    Time: 12 ms (62.06%), Space: 10.7 MB (39.27%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int countSubstrings(string s) {
+        int n = s.size();
+        int dp[n][n];
+        int ans = 0;
+        for(int j = 0; j < n; j++)
+        {
+            for(int i = 0, k = j; i < n && k < n; i++, k++)
+            {
+                    if(i == k)
+                    {
+                        dp[i][k] = 1;
+                    }
+                    else if(abs(i-k) == 1)
+                    {
+                        if(s[i] == s[k]) 
+                            dp[i][k] = 1;
+                        else 
+                            dp[i][k] = 0;
+                    }
+                    else
+                    {
+                        if(s[i] == s[k])
+                        {
+                            dp[i][k] = dp[i+1][k-1];
+                        }
+                        else
+                        {
+                            dp[i][k] = 0;
+                        }
+                    }
+                ans += dp[i][k];
+                }
+        }
+        return ans;
+    }
+};
