9017adac3cf1ab1ed844ff4d565885215f9c61a5
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-11-08 04:00:25 +0530

    Time: 8 ms (91.75%), Space: 13.6 MB (64.05%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        //vector<int> vec(2);
-        int n = nums.size();
-        int start = 0;
-        int end = n-1;
+    int firstoccur(int start, int end, vector<int>& nums, int target)
+    {
         int res1 = -1;
         while(start <= end)
         {
-           int mid = start + (end - start)/2;
+            int mid = start + (end - start)/2;
             if(target == nums[mid])
             {
                 res1 = mid;
@@ -19,22 +16,34 @@ public:
             else if(target < nums[mid])
                 end = mid - 1;   
         }
+        return res1;
+    }
+    
+    int lastoccur(int start, int end, vector<int>& nums, int target)
+    {
         int res2 = -1;
-        start = 0, end = n - 1;
-        
-        while(start <= end) 
+        while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if(nums[mid] == target)
+            if(target == nums[mid])
             {
-                res2 = mid;  
-                start = mid + 1;    
-            }
-            else if(nums[mid] >= target)
-                end = mid - 1;
-            else
+                res2 = mid;
                 start = mid + 1;
+            }
+            else if(target > nums[mid])
+                start = mid + 1;
+            else if(target < nums[mid])
+                end = mid - 1;   
         }
-        return {res1,res2};
+        return res2;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //vector<int> vec(2);
+        int n = nums.size();
+        int resf = -1, resl = -1;
+        resf = firstoccur(0, n - 1, nums, target);
+        resl = lastoccur(0, n - 1, nums, target);
+        return {resf, resl};
     }
 };
