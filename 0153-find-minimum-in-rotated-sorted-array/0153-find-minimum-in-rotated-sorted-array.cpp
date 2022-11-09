0e6dd57ba5d45fe27c2db8702ebfd7909f2a02b6
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-11-10 02:24:41 +0530

    Time: 3 ms (91.01%), Space: 10.1 MB (71.11%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,31 +2,51 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         
-        int n = nums.size();
+//         int n = nums.size();
     
-        if(n == 1)
-            return nums[0];
+//         if(n == 1)
+//             return nums[0];
         
-        if(nums[0] < nums[n-1])
-            return nums[0];
+//         if(nums[0] < nums[n-1])
+//             return nums[0];
+        
+//         int start = 0;
+//         int end = n-1;
+        
+//         int ans = INT_MAX;
+//         while(start <= end)
+//         {    
+//             int mid = (start + end) / 2;
+			
+//             if(nums[mid] >= nums[0])
+//                 start = mid +1;
+            
+//             else
+//             {
+//                 ans = min(ans , nums[mid]);
+//                 end = mid - 1;
+//             }
+//         }
+//         return ans;
+        
         
         int start = 0;
-        int end = n-1;
-        
-        int ans = INT_MAX;
+        int n = nums.size();
+        int end = n - 1, next, prev;
         while(start <= end)
-        {    
-            int mid = (start + end) / 2;
-			
-            if(nums[mid] >= nums[0])
-                start = mid +1;
-            
-            else
-            {
-                ans = min(ans , nums[mid]);
+        {
+            int mid = start + (end - start)/2;
+            if(nums[start] <= nums[end])
+                return nums[start];
+            next = (mid + 1) % n;
+            prev = (mid + n - 1) % n;
+            if(nums[mid] <= nums[next] && nums[mid] <= nums[prev])
+                return nums[mid];
+            else if(nums[start] <= nums[mid])
+                start = mid + 1;
+            else if(nums[mid] <= nums[end])
                 end = mid - 1;
-            }
         }
-        return ans;
+        return nums[start];
     }
 };
