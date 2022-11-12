2a37eeb04f99b1fa7cb4e9a112bb580b493d327b
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-11-12 18:37:55 +0530

    Time: 321 ms (99.03%), Space: 117 MB (88.90%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,58 @@
+class MedianFinder {
+public:
+    
+    priority_queue<int> small;  
+    priority_queue<int, vector<int>, greater<int>> large; //hs half larger elements 
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        
+        small.push(num);
+        
+        // if largest element of smaller is greater than the smallest element of larger, then push the top of smaller into larger
+        if(small.size() > 0 && large.size() > 0 && small.top() > large.top())
+        {
+            int ele = small.top();
+            small.pop();
+            large.push(ele);
+        }
+        
+        if(small.size() > large.size() + 1) // if the size of small>large by 2 then push                                               //the top of small into large
+        {
+            int ele = small.top();
+            small.pop();
+            large.push(ele);
+        }
+        else if(large.size() > small.size() + 1)
+        {
+            int ele = large.top(); 
+            large.pop();
+            small.push(ele);
+        }
+    }
+    
+    double findMedian() {
+        
+        if(small.size() == large.size())
+        {
+            return (double(small.top()) + double(large.top())) / 2;
+        }
+        else if(small.size() == large.size() + 1)
+        {
+            return double(small.top());
+        }
+        else
+        {
+            return double(large.top());
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
