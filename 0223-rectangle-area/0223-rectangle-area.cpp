01ba3d9be11cc8ee89c43b5526bbd0e6b612c9ea
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-11-17 16:49:29 +0530

    Time: 9 ms (79.08%), Space: 5.9 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-      int area1 = (ax2 - ax1) * (ay2 - ay1);     
-      int area2 = (bx2 - bx1) * (by2 - by1);              
-    
-      if (ax1 >= bx2 || ax2 <= bx1 || ay2 <= by1 || ay1 >= by2) 
-        return area1 + area2;                                               
-    
-    return area1 + area2 - (min(bx2, ax2) - max(ax1, bx1)) * (min(ay2, by2) - max(ay1, by1));  
+     int total_area = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1);
+     if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) 
+     {
+         return total_area; 
+     }
+        
+     int ar1 = min(ax2, bx2) - max(ax1, bx1);
+     int ar2 = min(ay2, by2) - max(ay1, by1);
+     int overlap_area = ar1 * ar2;
+     return total_area - overlap_area;
     }
 };
