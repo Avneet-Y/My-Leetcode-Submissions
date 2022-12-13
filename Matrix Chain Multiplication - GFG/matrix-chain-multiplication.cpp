1478fefd7ae5efdc1f902baef36c8efdd69315ab
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-13 23:02:31 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -7,45 +7,81 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+/*
 class Solution{
 public:
-    int t[501][501];
 
-int solve(int arr[],int i,int j){
-
-    if(i>=j) return 0;
-
-    if(t[i][j]!=-1) return t[i][j];
-
-    int mn=INT_MAX;
-
-    for(int k=i;k<j;k++){
-
-        int temp=solve(arr,i,k)+solve(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
-
-        mn=min(mn,temp); 
-
-    }
-
-      return t[i][j]=mn;
-
-}
-
-    int matrixMultiplication(int N, int arr[])
-
+    int dp[501][501];
+    int solve(int arr[], int i, int j)
     {
-
-        // code here
-
-        memset(t,-1,sizeof(t));
-
-        int i=1;
-
-        int j=N-1;
-
-        return solve(arr,i,j);
+        if(i >= j)
+            return 0;
+            
+        int mini = INT_MAX;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        for(int k = i+1; k <= j; k++)
+        {
+            int tempans = solve(arr, i, k-1) + solve(arr, k, j) + arr[i-1]*arr[k-1]*arr[j];
+        
+            if(tempans < mini)
+            {
+                mini = tempans;
+            }
+        }
+        return dp[i][j] = mini;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        int mincost;
+        memset(dp, -1, sizeof(dp));
+        mincost = solve(arr, 1, N-1);
+        return mincost;
     }
 };
+
+*/
+
+
+
+
+
+
+
+
+
+
+class Solution{
+public:
+
+    int dp[501][501];
+    int solve(int A[], int i, int j)
+    {
+        if(i >= j)
+          return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int ans = INT_MAX;
+        for(int k = i; k < j; k++)
+        {
+            int temp = solve(A, i, k) + solve(A, k+1, j) + A[i-1]*A[k]*A[j];
+            if(temp < ans)
+                ans = temp;
+        }
+        return dp[i][j] = ans;
+    }
+   
+    int matrixMultiplication(int N, int arr[])
+    {
+        int sol;
+        memset(dp, -1, sizeof(dp));
+        sol = solve(arr, 1, N-1);
+        return sol;
+    }
+};
+
 
 //{ Driver Code Starts.
 
