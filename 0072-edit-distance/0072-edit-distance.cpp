797bce09a3527718511cf0c1feb9de8514de8b6a
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-14 17:50:14 +0530

    Time: 172 ms (8.91%), Space: 80.3 MB (13.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     
@@ -31,6 +32,33 @@ public:
         int n = word1.size();
         int m = word2.size();
         int ans = lcs(word1, word2, n, m);
+        return ans;
+    }
+};
+*/
+
+class Solution {
+public:
+    int dp[501][501];
+    int solve(string s1, string s2, int i, int j)
+    {
+        if(i < 0)
+            return j+1;
+        if(j < 0)
+            return i+1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(s1[i] == s2[j])
+            return dp[i][j] = solve(s1, s2, i-1, j-1);
+        else
+            return dp[i][j] = 1 +  min(solve(s1, s2, i-1, j), min(solve(s1, s2, i, j-1), solve(s1, s2, i-1, j-1)));
+    }
+    
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(word1, word2, n-1, m-1);
         return ans;
     }
 };
