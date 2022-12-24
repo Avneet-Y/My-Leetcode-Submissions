c30ff0b17a221c262e86ce1488b117368576b4d8
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-25 01:12:45 +0530

    Time: 25 ms (57.23%), Space: 11.9 MB (62.91%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -41,5 +42,58 @@ public:
         }
         return sell[k-1];
         
+    }
+};
+
+*/
+
+
+class Solution {
+public:
+    // if state == true => we have to buy the stock
+    // if state == false => we have to sell the stock
+    // initially state will be true, b/c first we have to buy the stock 
+    // declare a dp
+    
+    int dp[1005][105][2];
+    
+    int dfs(vector<int>& prices, int i, int n, int k, bool state)
+    { 
+        if(k < 0)   // if there is no. transactions remaining  
+            return 0;
+        
+        if(i == n)  //if there is no. element remaining
+        {
+            return 0;
+        }
+        
+        
+        if(dp[i][k][state] != -1)
+            return dp[i][k][state];
+        
+        int maxi = INT_MIN;
+        
+        // we have two option either include the curr_element or exclude
+        // inclusion part (either we buying or selling depends on state)
+        
+        if(state)
+        {
+            maxi = max(maxi, - prices[i] + dfs(prices, i + 1, n, k - 1, !state));
+        }
+        else
+        {
+            maxi = max(maxi, prices[i] + dfs(prices, i + 1, n, k, !state));
+        }
+        
+        // exclusion part
+        maxi = max(maxi, dfs(prices, i + 1, n, k, state));
+        return dp[i][k][state] = maxi;  // store the res then return 
+    }
+    
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n = prices.size();
+        memset(dp, -1, sizeof(dp));
+        return dfs(prices, 0, n, k, true);
     }
 };
