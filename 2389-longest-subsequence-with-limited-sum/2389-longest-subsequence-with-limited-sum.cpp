4af0bc6c29bcd7d062751d681b5c7634eee54738
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-25 13:45:11 +0530

    Time: 37 ms (70.50%), Space: 13.8 MB (36.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -5,23 +5,27 @@ public:
         int n = nums.size();
         int m = queries.size();  
         vector<int> answer;
+        vector<int> prefix(n);
         sort(nums.begin(), nums.end());
         
+        int sum = 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            sum += nums[i];
+            prefix[i] = sum;
+        }
         for(int i=0; i<m; i++)
         {
-            int sum = 0;
-            int count = 0;
+            int count = 0; 
             for(int j=0; j<n; j++)
             {
-                sum += nums[j];
-                if(sum <= queries[i])
-                {
-                   count++; 
-                }
-                else 
+                if(prefix[j] <= queries[i])
+                    count++;
+                else
                     break;
             }
-            answer.push_back(count);
+            answer.push_back(count);    
         }
         return answer;                                                             
     }
