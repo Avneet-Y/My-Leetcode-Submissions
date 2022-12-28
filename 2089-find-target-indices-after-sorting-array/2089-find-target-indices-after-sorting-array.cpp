2a4c94d113dab4e6c0699b9bf584a958a98c4c53
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-28 17:48:35 +0530

    Time: 8 ms (66.63%), Space: 11.8 MB (19.40%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,28 +1,123 @@
 class Solution {
 public:
-     vector<int> ans;
-    void binary(vector <int> nums, int start, int end, int target)
+    int first(vector<int> &nums, int start, int end, int ele)
     {
-        if(start > end)       
-            return;
-        
-        int mid = start + (end-start)/2;       
-        if(nums[mid] == target)
-        {                
-            binary(nums, start, mid-1, target);
-            ans.push_back(mid);
-            binary(nums, mid+1, end, target);      
+        int res1 = -1;
+        while(start <= end)
+        {
+            int mid = start + (end-start/2);
+            if(ele == nums[mid])
+            {
+                res1 = mid;
+                end = mid - 1;
+            }
+            else if(ele > nums[mid])
+                start = mid+1;
+            else
+                end = mid - 1;
         }
-        else if(nums[mid]>target)               
-            binary(nums, start, mid-1, target);
-        else                                    
-            binary(nums, mid+1, end, target);
+        return res1;
     }
-    vector<int> targetIndices(vector<int>& nums, int target) {
+    int last(vector<int> &nums, int start, int end, int ele)
+    {
+        int res2 = -1;
         
-        sort(nums.begin(), nums.end());     
-        int start=0, end=nums.size()-1;
-        binary(nums, start, end, target);   
+        while(start <= end)
+        {
+            int mid = start + (end-start/2);
+            if(ele == nums[mid])
+            {
+                res2 = mid;
+                start = mid + 1;
+            }   
+            else if(ele > nums[mid])
+                start = mid+1;
+            else
+                end = mid - 1;
+        }
+        return res2;
+    }
+    
+    
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int fr = first(nums, 0, n-1, target);
+        int lt = last(nums, 0, n-1, target);
+        
+        if(fr == -1 || lt == -1)
+            return {};
+        else if(fr == lt)
+            ans.push_back(fr);
+        else
+        {
+            for(int i = fr; i<= lt; i++)
+                ans.push_back(i);
+        }
         return ans;
+                                                                                        
     }
 };
+
+
+
+
+
+                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//      vector<int> ans;
+//     void binary(vector <int> nums, int start, int end, int target)
+//     {
+//         if(start > end)       
+//             return;
+        
+//         int mid = start + (end-start)/2;       
+//         if(nums[mid] == target)
+//         {                
+//             binary(nums, start, mid-1, target);
+//             ans.push_back(mid);
+//             binary(nums, mid+1, end, target);      
+//         }
+//         else if(nums[mid]>target)               
+//             binary(nums, start, mid-1, target);
+//         else                                    
+//             binary(nums, mid+1, end, target);
+//     }
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+        
+//         sort(nums.begin(), nums.end());     
+//         int start=0, end=nums.size()-1;
+//         binary(nums, start, end, target);   
+//         return ans;
+//     }
+// };
