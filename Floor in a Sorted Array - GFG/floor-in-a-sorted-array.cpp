b8a5f75042f67a82508e473114c1b657c2abc485
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-29 20:22:46 +0530

    Added solution - LeetHub

--- Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
@@ -12,7 +12,6 @@ class Solution{
     
     int solve(vector<long long> v, int start, int end, int ele)
     {
-        int res = -1;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
@@ -22,16 +21,14 @@ class Solution{
             }
             else if(v[mid] < ele)
             {
-                res = mid;
                 start = mid+1;
-                
             }
             else if(v[mid] > ele)
             {
                 end = mid - 1;
             }
         }
-        return res;
+        return end;
     }
     
     int findFloor(vector<long long> v, long long n, long long x){
