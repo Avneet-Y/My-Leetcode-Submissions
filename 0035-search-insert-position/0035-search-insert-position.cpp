5485a43d5c169f23ca7487c156709749f8ec178f
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2022-12-29 20:24:42 +0530

    Time: 6 ms (73.78%), Space: 9.7 MB (73.96%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,4 +1,5 @@
 //alterntive question to find ceil of an element
+/*
 
 class Solution {
 public:
@@ -30,6 +31,40 @@ public:
         int ans = solve(nums, 0, n-1, target);
         if(ans == -1)
             return n;
+        return ans;
+    }
+};
+*/
+
+//or simple binary search and return start
+class Solution {
+public:
+    int solve(vector<int>& nums, int start, int end, int target)
+    {
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = solve(nums, 0, n-1, target);
+        // if(ans == -1)
+        //     return n;
         return ans;
     }
 };
