ee38ab24557dbdd0db0cd37996a74674c9e37b94
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-03 01:35:04 +0530

    Time: 133 ms (31.15%), Space: 26.1 MB (30.65%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    bool isvalid(vector<int>& weights, int n, int days, int maxi)
+    {
+        int temp = 1;
+        int sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += weights[i];
+            if(sum > maxi)
+            {
+                temp++;
+                sum = weights[i];
+            }
+            if(temp > days)
+                return false;
+        }
+        return true;
+    }
+    
+    int solve(vector<int>& weights, int n, int days)
+    {
+        int sum = 0, res = -1;
+        int maxi = INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            sum += weights[i];
+            maxi = max(maxi, weights[i]);
+        }
+        int start = maxi;
+        int end = sum;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(isvalid(weights, n, days, mid) == true)
+            {
+                res = mid;
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int ans = solve(weights, n, days);
+        return ans;
+    }
+};
