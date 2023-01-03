8ce99361e6a0e4b0de238667fe58af2181c219ae
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-03 22:49:05 +0530

    Time: 126 ms (25.91%), Space: 18.9 MB (38.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,29 +3,28 @@ public:
     
     bool isvalid(vector<int>& piles, int h, long long mid)
     {
-        int count = 0;
+        double time = 0;
         for(int i=0; i<piles.size(); i++)
         {
-                count += piles[i]/mid;
-                if(piles[i] % mid != 0)
-                    count++;
+                time += ceil(piles[i]*1.0/mid);
         }
-        if(count <= h) 
+        if(time <= h) 
             return true;
         return false;
     }   
     
     int solve(vector<int>& piles, int n, int h)
     {
-        long long sum = 0, res = -1;
+        long long res = -1;
         int maxi = INT_MIN;
         for(int i=0; i<n; i++)
         {
-            sum += piles[i];
             maxi = max(maxi, piles[i]);
         }
-        long long start = 1;
-        long long end = sum;
+        if(h == n)
+            return maxi;
+        long long start = 0;
+        long long end = maxi;
         while(start <= end)
         {
             long long mid = start + (end - start)/2;
