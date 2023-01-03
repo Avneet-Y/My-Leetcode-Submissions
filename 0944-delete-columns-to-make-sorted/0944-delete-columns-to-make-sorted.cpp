bc5d42f874e95dda826835b30a5f0e23eb9af5df
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-03 18:28:38 +0530

    Time: 110 ms (41.62%), Space: 12.1 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,20 +3,35 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int n = strs.size();
         int m = strs[0].size();
-        int del = 0;
+//         int del = 0;
         
-        for(int j=0; j<m; j++)
+//         for(int j=0; j<m; j++)
+//         {
+//             string currentcol = "";
+//             for(int i=0; i<n; i++)
+//             {
+//                 currentcol += strs[i][j];   
+//             }
+//             string revcol = currentcol;
+//             sort(currentcol.begin(), currentcol.end());
+//             if(currentcol != revcol)
+//                 del++;
+//         }
+//         return del;
+        
+        int ct = 0; 
+        
+        for(int i=0; i<m; i++)
         {
-            string currentcol = "";
-            for(int i=0; i<n; i++)
+            bool flag = false;
+            for(int j=1; j<n; j++)
             {
-                currentcol += strs[i][j];   
+                if(strs[j][i] < strs[j-1][i])
+                    flag = true;    //mark true if found in non decreasing order
             }
-            string revcol = currentcol;
-            sort(currentcol.begin(), currentcol.end());
-            if(currentcol != revcol)
-                del++;
+            if(flag)
+                ct++;
         }
-        return del;
+        return ct;
     }
 };
