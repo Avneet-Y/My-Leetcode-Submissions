485ab6399c56a4321d3449681c0e6b9699d13fa4
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-04 20:13:37 +0530

    Time: 336 ms (55.68%), Space: 103.8 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,19 +1,55 @@
+// class Solution {
+// public:
+//     int minimumRounds(vector<int>& tasks) {
+//         int n = tasks.size(); 
+//         unordered_map<int, int> mp;
+//         int count = 0;
+//         for(auto i : tasks)
+//         {
+//             mp[i]++;
+//         }
+//         for(auto it = mp.begin(); it != mp.end(); it++)
+//         {
+//             if(it->second == 1)
+//                 return -1;
+//             count += ceil(it->second/3.0);
+//         }
+//         return count;
+//     }
+// }; 
+
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
         int n = tasks.size(); 
         unordered_map<int, int> mp;
-        int count = 0;
+        int ans = 0;
         for(auto i : tasks)
         {
             mp[i]++;
         }
         for(auto it = mp.begin(); it != mp.end(); it++)
         {
-            if(it->second == 1)
+            int count = it->second;
+            if(count == 1)
+            {
                 return -1;
-            count += ceil(it->second/3.0);
+            }
+            if(count % 3 == 0)
+            {
+                ans += count / 3;
+            }
+            else if(count % 3 == 2)
+            {
+                ans += count / 3;
+                ans++;
+            }
+            else
+            {
+                ans += count / 3;
+                ans++;
+            }
         }
-        return count;
+        return ans;
     }
 }; 
