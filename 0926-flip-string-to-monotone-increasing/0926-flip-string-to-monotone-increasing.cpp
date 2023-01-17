171725f011deb0e67e66160fc166931323e0b90c
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-17 18:23:12 +0530

    Time: 20 ms (93.67%), Space: 11.1 MB (52.66%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,22 +2,34 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        int flip = 0, minflip = 0;
-        for(int i = 0; i < n; i++)
-        {
-            flip += (s[i]-'0');  // count '1's in given string
-        }
-        minflip = flip;
-        for(int i = n-1; i >=0 ; i --)
-        {
-            if(s[i] == '1') // considered flip in last sequence
-                flip -= 1;
-            else // not considered flip in last sequence
-                flip += 1;
+//         int flip = 0, minflip = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             flip += (s[i]-'0');  // count '1's in given string
+//         }
+//         minflip = flip;
+//         for(int i = n-1; i >=0 ; i --)
+//         {
+//             if(s[i] == '1') // considered flip in last sequence
+//                 flip -= 1;
+//             else // not considered flip in last sequence
+//                 flip += 1;
 				
-            minflip = min(minflip, flip); 
-        }
-        return minflip;
+//             minflip = min(minflip, flip); 
+//         }
+//         return minflip;
         
+        int flip = 0, count1 = 0;
+        for(int i = 0; i<n; i++)
+        { 
+            if (s[i] == '1')
+                count1++;
+            else
+            {
+                flip++;
+                flip = min(flip, count1);
+            }
+        }
+        return flip;   
     }
 };
