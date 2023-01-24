36f7b871ef870570a6b3de88f139ee13da36c689
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-01-24 23:55:46 +0530

    Time: 62 ms (24.03%), Space: 18.7 MB (12.87%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    vector<int> findCoordinates(int currVal, int n) 
+    vector<int> findcoord(int curVal , int n)
     {
-        //calculate row by subtracting the quotient of (curr-1) / n from n-1. done to convert the 1-indexed position of the element to a 0-indexed position in the board.
-        int row = n - (currVal - 1) / n - 1;
-        int col = (currVal - 1) % n;
+        int row = n - (curVal - 1) / n - 1;
+        int col = (curVal - 1) % n;
         if (row % 2 == n % 2) 
         {
             return {row, n - 1 - col};
@@ -14,45 +13,40 @@ public:
             return {row, col};
         }
     }
-        
+    
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        int moves = 0;
+        int n  = board.size();
+        int steps = 0;
         queue<int> q;
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        q.push(1);
+        vector<vector<bool>> visited(n, vector<bool> (n, false));
         visited[n-1][0] = true;
+        q.push(1);
         while(!q.empty())
         {
             int size = q.size();
             for(int i = 0; i < size; i++)
             {
-                int currBoardVal = q.front();
+                int currnode = q.front();
                 q.pop();
-                if(currBoardVal == n*n) 
-                    return moves;
-                for(int diceVal = 1; diceVal <= 6; diceVal++)
+                if(currnode == n*n)
+                    return steps;
+                for(int diceval = 1; diceval <= 6; diceval++)
                 {
-                    if(currBoardVal + diceVal > n*n) 
+                    if(diceval + currnode > n*n)
                         break;
-                    vector<int> pos = findCoordinates(currBoardVal + diceVal, n);
-                    int row = pos[0];
-                    int col = pos[1];
-                    if(visited[row][col] == false)
-                    {
-                        visited[row][col] = true;
-                        if(board[row][col] == -1)
-                        {
-                            q.push(currBoardVal + diceVal);
-                        }
-                        else
-                        {
-                            q.push(board[row][col]);
-                        }
-                    }
+                    vector<int> pos = findcoord(diceval + currnode, n);
+                    int r = pos[0];
+                    int c = pos[1];
+                    if(visited[r][c] == true)
+                        continue;
+                    visited[r][c] = true;
+                    if(board[r][c] == -1)
+                        q.push(diceval + currnode);
+                    else
+                        q.push(board[r][c]);
                 }
             }
-            moves++;
+            steps++;
         }
         return -1;
     }
