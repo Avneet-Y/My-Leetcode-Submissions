cae082d73a0c9241ab52bf04692d68a61560a4c2
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-01 01:11:43 +0530

    Time: 195 ms (70.55%), Space: 19.9 MB (72.49%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,4 +1,4 @@
-/*
+
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
@@ -25,38 +25,6 @@ public:
         return ans;
     }
 };
-*/
 
 
-class Solution {
-public:
-int dp[1001][1001];
-    int rec(vector<pair<int,int>>& vec, int i, int prev, int age)
-    {
-        if(i >= vec.size())
-            return 0;
 
-        if(dp[i][prev+1] != -1)
-            return dp[i][prev+1];
-        
-        int take = 0, notTake = 0;
-        if(prev == -1 || vec[i].second >= vec[prev].second)
-        {
-            take = vec[i].second + rec(vec, i+1, i, vec[i].first);
-        }
-        notTake = rec(vec, i+1, prev, vec[i].first);
-        return dp[i][prev+1] = max(notTake, take);
-    }
-
-    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        memset(dp, -1, sizeof(dp));
-        vector<pair<int,int>> vec;
-        int n = scores.size();
-        for(int i=0; i<n; i++)
-        {
-            vec.push_back({ages[i],scores[i]});
-        }
-        sort(vec.begin(),vec.end());
-        return rec(vec, 0, -1, vec[0].first);
-    }
-};
