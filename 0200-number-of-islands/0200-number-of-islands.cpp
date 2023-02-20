a06ee710025db918713c6ff5ae9b1b35d7b998d2
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-21 02:12:23 +0530

    Time: 56 ms (23.44%), Space: 19.2 MB (13.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    bool isvalid(vector<vector<char>>& grid, int i, int j, int n, int m)
+    void bfs(int row, int col, int n, int m, vector<vector<char>>& grid, vector<vector<int>>& visited)
     {
-        if(i >= 0 && i < n && j >= 0 && j<m && grid[i][j] == '1')
-            return true;
-        return false;
-    }
-    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m)
-    {
-        grid[i][j] = '0';
-        if(isvalid(grid, i+1, j, n, m))
-            dfs(grid, i+1, j, n, m);
-        
-        if(isvalid(grid, i-1, j, n, m))
-            dfs(grid, i-1, j, n, m);
-        
-        if(isvalid(grid, i, j+1, n, m))
-            dfs(grid, i, j+1, n, m);
-        
-        if(isvalid(grid, i, j-1, n, m))
-            dfs(grid, i, j-1, n, m);
+        visited[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+      
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++)
+            {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(ncol >= 0 && ncol < m && nrow >= 0 && nrow < n && visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1')
+                {
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        
         int n = grid.size();
         int m = grid[0].size();
         int count = 0;
-        
+        vector<vector<int>> visited(n , vector<int> (m, 0));
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j] == '1')
+                if(grid[i][j] == '1' && visited[i][j] == 0)
                 {
                     count++;
-                    dfs(grid, i, j, n, m);
+                    bfs(i, j, n, m, grid, visited);
                 }
             }
         }
