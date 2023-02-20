89a1362566b9703007e53dbe08d8895cddb52885
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-20 19:38:28 +0530

    Time: 10 ms (22.58%), Space: 9.7 MB (77.52%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,70 +1,28 @@
-//alterntive question to find ceil of an element
-/*
-
 class Solution {
 public:
-    int solve(vector<int>& nums, int start, int end, int target)
-    {
-        int res = -1;
-        while(start <= end)
-        {
-            int mid = start + (end-start)/2;
-            if(nums[mid] == target)
-            {
-                return mid;
-            }
-            else if(nums[mid] > target)
-            {
-                res = mid;
-                end = mid - 1;
-            }
-            else if(nums[mid] < target)
-            {
-                start = mid + 1;
-            }
-        }
-        return res;
-    }
-    
-    int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int ans = solve(nums, 0, n-1, target);
-        if(ans == -1)
-            return n;
-        return ans;
-    }
-};
-*/
-
-//or simple binary search and return start
-class Solution {
-public:
-    int solve(vector<int>& nums, int start, int end, int target)
+    int search(vector<int>& nums, int start, int end, int target)
     {
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            if(nums[mid] == target)
-            {
+            if(target == nums[mid])
                 return mid;
-            }
-            else if(nums[mid] > target)
+            else if(target > nums[mid])
             {
-                end = mid - 1;
+                start = mid+1;
             }
-            else if(nums[mid] < target)
+            else if(target < nums[mid])
             {
-                start = mid + 1;
+                end = mid-1;
             }
         }
         return start;
+        
     }
-    
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
-        int ans = solve(nums, 0, n-1, target);
-        // if(ans == -1)
-        //     return n;
+        int ans = search(nums, 0, n-1, target);
         return ans;
+        
     }
 };
