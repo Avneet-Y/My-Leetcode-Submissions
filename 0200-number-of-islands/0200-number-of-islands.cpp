320a9d0299bddc70eb252fb8e23477b09d9e1ac1
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-21 01:03:17 +0530

    Time: 26 ms (99.54%), Space: 12.4 MB (66.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,109 +1,45 @@
-/*
-
 class Solution {
 public:
-    
-    bool isvalid(int i, int j, int n, int m, vector<vector<char>>& grid)
+    bool isvalid(vector<vector<char>>& grid, int i, int j, int n, int m)
     {
-        if(i>=0 and i<n and j>=0 and j<m and grid[i][j]=='1')
+        if(i >= 0 && i < n && j >= 0 && j<m && grid[i][j] == '1')
             return true;
-        else
-            return false;
+        return false;
     }
-    
-    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid)
+    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m)
     {
         grid[i][j] = '0';
+        if(isvalid(grid, i+1, j, n, m))
+            dfs(grid, i+1, j, n, m);
         
-        if (isvalid(i+1, j, n, m, grid))
-            dfs(i+1, j, n, m, grid);
+        if(isvalid(grid, i-1, j, n, m))
+            dfs(grid, i-1, j, n, m);
         
-        if (isvalid(i, j+1, n, m, grid))
-            dfs(i,j+1,n,m,grid);
+        if(isvalid(grid, i, j+1, n, m))
+            dfs(grid, i, j+1, n, m);
         
-        if (isvalid(i-1, j, n, m, grid))
-            dfs(i-1, j, n, m, grid);
-        
-        if (isvalid(i, j-1, n, m, grid))
-            dfs(i, j-1, n, m, grid);
-        
-        }
-    
-    
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int count1 = 0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1'){
-                count1++;
-                dfs(i,j,n,m,grid);
-                }
-            }
-        }
-        return count1;
-    }
-};
-
-*/
-class Solution {
-public:    
-    
-    void bfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>>& grid)
-    {
-        visited[row][col] = 1;
-        queue<pair<int, int>> q;
-        q.push({row, col});
-        int n = grid.size();
-        int m = grid[0].size();
-        while(q.size() > 0)
-        {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int delrow = -1; delrow <= 1; delrow++)
-            {
-                for(int delcol = -1; delcol <= 1; delcol++)
-                {
-                    //not to check the diagonal elements.
-                    if((delrow == -1 && delcol == -1) || (delrow == -1 && delcol == 1) 
-                       || (delrow == 1 && delcol == -1) || (delrow == 1 && delcol == 1))
-                    {
-                        continue;
-                    }
-                    int nrow = row + delrow;
-                    int ncol = col + delcol;
-                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-                       && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-                    {
-                        visited[nrow][ncol] = 1;
-                        q.push({nrow, ncol});
-
-                    }
-                }
-            }
-        }
+        if(isvalid(grid, i, j-1, n, m))
+            dfs(grid, i, j-1, n, m);
     }
     
     int numIslands(vector<vector<char>>& grid) {
+        
         int n = grid.size();
         int m = grid[0].size();
         int count = 0;
-        vector<vector<int>> visited(n, vector<int> (m, 0));
+        
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j] == '1' && visited[i][j] == 0)
+                if(grid[i][j] == '1')
                 {
                     count++;
-                    bfs(i, j, visited, grid);
+                    dfs(grid, i, j, n, m);
                 }
             }
         }
-        return count; 
+        return count;
+        
     }
 };
