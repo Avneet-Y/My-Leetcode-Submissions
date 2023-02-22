afa641a3056a1fce75934f244afda5327b7c2afb
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-22 14:34:52 +0530

    Time: 64 ms (62.89%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,9 +1,7 @@
-//similar to allocate minimum number of pages
-
 class Solution {
 public:
     
-    bool isvalid(vector<int>& weights, int n, int days, int maxi)
+    bool isvalid(vector<int>& weights, int days, int maxi, int n)
     {
         int tempdays = 1;
         int sum = 0;
@@ -21,36 +19,35 @@ public:
         return true;
     }
     
-    int solve(vector<int>& weights, int n, int days)
+    int search(vector<int>& weights, int days, int n)
     {
-        int sum = 0, res = -1;
+        int sum = 0;
         int maxi = INT_MIN;
+        int res = -1;
         for(int i=0; i<n; i++)
         {
             sum += weights[i];
-            maxi = max(maxi, weights[i]);
+            maxi = max(weights[i], maxi);
         }
         int start = maxi;
         int end = sum;
         while(start <= end)
         {
-            int mid = start + (end - start)/2;
-            if(isvalid(weights, n, days, mid) == true)
+            int mid = start + (end-start)/2;
+            if(isvalid(weights, days, mid, n) == true)
             {
                 res = mid;
-                end = mid - 1;
+                end = mid-1;
             }
             else
-            {
-                start = mid + 1;
-            }
+                start = mid+1;
         }
         return res;
     }
     
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int ans = solve(weights, n, days);
+        int ans = search(weights, days, n);
         return ans;
     }
 };
