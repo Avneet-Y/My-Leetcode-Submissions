03cf917469e32e8f737e4809eaf4384aed6ef8e0
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-02-26 12:38:07 +0530

    Time: 8 ms (87.54%), Space: 7.3 MB (79.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,68 +1,35 @@
-/*
-//Tabulation
-
 class Solution {
 public:
-    
-    int lcs(string s1, string s2, int n, int m)
+    int solvelcs(string word1, string word2, int n1, int n2)
     {
-        int dp[n+1][m+1];
-        for(int i = 0; i<n+1; i++)
+        int dp[n1+1][n2+1];
+        for(int i=0; i <= n1; i++)
         {
-            dp[i][0] = i;
+           dp[i][0] = i;
         }
-        for(int j = 0; j<m+1; j++)
+        for(int j=0; j <= n2; j++)
         {
             dp[0][j] = j;
         }
-        for(int i=1; i<n+1; i++)
+        for(int i=1; i <= n1; i++)
         {
-            for(int j=1; j<m+1; j++)
+            for(int j=1; j <= n2; j++)
             {
-                if(s1[i-1] == s2[j-1])
+                if(word1[i-1] == word2[j-1])
                 {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else
-                    dp[i][j] = 1 + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
             }
         }
-        return dp[n][m];
+        return dp[n1][n2];
     }
     
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        int ans = lcs(word1, word2, n, m);
-        return ans;
-    }
-};
-*/
-
-//Memoized
-
-class Solution {
-public:
-    int dp[501][501];
-    int solve(string s1, string s2, int i, int j)
-    {
-        if(i < 0)
-            return j+1;
-        if(j < 0)
-            return i+1;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s1[i] == s2[j])
-            return dp[i][j] = solve(s1, s2, i-1, j-1);
-        else
-            return dp[i][j] = 1 +  min(solve(s1, s2, i-1, j), min(solve(s1, s2, i, j-1), solve(s1, s2, i-1, j-1)));
-    }
-    
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(word1, word2, n-1, m-1);
+        int n1 = word1.size();
+        int n2 = word2.size();
+        int ans = solvelcs(word1, word2, n1, n2);
         return ans;
     }
 };
