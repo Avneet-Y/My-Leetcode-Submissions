fe563673ea86ec6c487ba6f527085c256c1e20b1
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-03-04 01:25:06 +0530

    Time: 26 ms (61.35%), Space: 7.8 MB (95.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int n1 = s.size();
+        int n2 =  t.size();
+        
+        int ans = INT_MAX;
         unordered_map<char, int> mp;
-        int slen = s.size();
-        int tlen = t.size();
-        for (int i = 0; i < tlen; i++)
-        {
-            mp[t[i]]++;
-        }
+        for(auto it : t)
+            mp[it]++;
+        
         int count = mp.size();
-        string ans;
-
-        int mini = INT_MAX;
-
+        int start = 0; 
+        
         int i = 0;
         int j = 0;
-
-        while (j < slen)
+        while(j < n1)
         {
-
-            if (mp.find(s[j]) != mp.end())
+            mp[s[j]]--;
+            if(mp[s[j]] == 0)
+                count--;
+            
+            if(count == 0)  //trying to minimize Win Size after getting a candidate for ans
             {
-                mp[s[j]]--;
-                if (mp[s[j]] == 0)
+                
+                while(count == 0)
                 {
-                    count--;
-                }
-            }
-
-            while (count == 0)
-            {
-                if (mini > j - i + 1)
-                {
-                    mini = min(mini, j - i + 1);
-                    ans = s.substr(i, j - i + 1);
-                }
-                if (mp.find(s[i]) != mp.end())
-                {
-                    mp[s[i]]++;
-                    if (mp[s[i]] == 1)
+                    if (ans > j - i + 1)
                     {
-                        count++;
+                        ans = j - i + 1; //updating the answer
+                        start = i;
                     }
+                    
+                    mp[s[i]]++;
+                    if (mp[s[i]] > 0)
+                        count++;
+                    i++;
                 }
-                i++;
             }
             j++;
         }
-        return ans;
+        if(ans != INT_MAX) 
+            return s.substr(start, ans);
+        else
+            return "";   
     }
 };
