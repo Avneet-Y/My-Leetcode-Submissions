f92abf91f92795605d1cd947afe8927f97dab9ea
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-03-04 01:49:18 +0530

    Time: 22 ms (69.51%), Space: 8 MB (71.46%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        
+        /*
+        //showing TLE
+        
+        unordered_map<char, int> mp;
+        int n1 = s.size();
+        int n2 = t.size();
+        for (int i = 0; i < n2; i++)
+        {
+            mp[t[i]]++;
+        }
+        int count = mp.size();
+        string ans;
+
+        int mini = INT_MAX;
+
+        int i = 0;
+        int j = 0;
+
+        while (j < n1)
+        {
+
+            if (mp.find(s[j]) != mp.end())
+            {
+                mp[s[j]]--;
+                if (mp[s[j]] == 0)
+                {
+                    count--;
+                }
+            }
+
+            while (count == 0)
+            {
+                if (mini > j - i + 1)
+                {
+                    mini = min(mini, j - i + 1);
+                    ans = s.substr(i, j - i + 1);
+                }
+                if (mp.find(s[i]) != mp.end())
+                {
+                    mp[s[i]]++;
+                    if (mp[s[i]] == 1)
+                    {
+                        count++;
+                    }
+                }
+                i++;
+            }
+            j++;
+        }
+        return ans;
+      */
+        
         int n1 = s.size();
         int n2 =  t.size();
         
@@ -20,9 +73,8 @@ public:
             if(mp[s[j]] == 0)
                 count--;
             
-            if(count == 0)  //trying to minimize Win Size after getting a candidate for ans
+            if(count == 0) //trying to minimize Win Size after getting a candidate for ans
             {
-                
                 while(count == 0)
                 {
                     if (ans > j - i + 1)
@@ -42,6 +94,7 @@ public:
         if(ans != INT_MAX) 
             return s.substr(start, ans);
         else
-            return "";   
+            return "";
+   
     }
 };
