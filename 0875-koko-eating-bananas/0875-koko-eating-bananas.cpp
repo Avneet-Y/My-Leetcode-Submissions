080db88216f195c56dc5d7cef82f6aef4aead650
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-03-08 14:25:23 +0530

    Time: 42 ms (88.59%), Space: 19 MB (11.65%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    
     bool isvalid(vector<int>& piles, int h, long long mid)
     {
+        int n = piles.size();
         double time = 0;
-        for(int i=0; i<piles.size(); i++)
+        for(int i=0; i < n; i++)
         {
-                time += ceil(piles[i]*1.0/mid);
+            time += ceil(piles[i]*1.0/mid);
+            
         }
-        if(time <= h) 
+        if(time <= h)
             return true;
         return false;
-    }   
+    }
     
-    int solve(vector<int>& piles, int n, int h)
+    int bsearch(vector<int>& piles, int n, int h)
     {
-        long long res = -1;
-        int maxi = INT_MIN;
-        for(int i=0; i<n; i++)
-        {
-            maxi = max(maxi, piles[i]);
-        }
-        if(h == n)
-            return maxi;
+        
+        long long maxi = *max_element(piles.begin(), piles.end());
         long long start = 0;
         long long end = maxi;
-        while(start <= end)
+        if(h == n)
+            return maxi;
+        
+        long long res = -1;
+        
+        while(start <=  end)
         {
-            long long mid = start + (end - start)/2;
-            if(isvalid(piles, h, mid) == true)
-            {
+            long long mid = start + (end - start) / 2;
+            if(isvalid(piles, h, mid) == true){
                 res = mid;
                 end = mid - 1;
             }
-            else
-            {
+            else if(isvalid(piles, h, mid) == false){
                 start = mid + 1;
             }
         }
@@ -42,9 +40,8 @@ public:
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
-        
         int n = piles.size();
-        int ans = solve(piles, n, h);
+        int ans = bsearch(piles, n, h);
         return ans;
         
     }
