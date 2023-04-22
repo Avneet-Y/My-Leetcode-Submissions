0a2bb00d88294970702555240e725e77fda73122
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-04-22 22:20:39 +0530

    Time: 39 ms (84.65%), Space: 7.6 MB (70.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    
-    int lcs(string str1, string str2, int m, int n)
+    int dp[501][501];
+    int lcs(string s1, string s2, int n, int m)
     {
-        int dp[m+1][n+1];
-        for(int i = 0; i < m+1; i++)
+        for(int i=0; i<n+1; i++)
         {
-            for(int j = 0; j < n+1; j++)
+            for(int j=0; j<m+1; j++)
             {
                 if(i == 0 || j == 0)
+                {
                     dp[i][j] = 0;
+                }
             }
         }
-        for(int i = 1; i < m+1; i++)
+        for(int i=1; i<n+1; i++)
         {
-            for(int j = 1; j < n+1; j++)
+            for(int j=1; j<m+1; j++)
             {
-                if(str1[i-1] == str2[j-1])
+                if(s1[i-1] == s2[j-1])
+                {
                     dp[i][j] = 1 + dp[i-1][j-1];
+                }
                 else
+                {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
     
     int minInsertions(string s) {
-        
-        int m = s.size();
-        string st2 = s;
+        int n = s.size();
+        string s2 = s;
         reverse(s.begin(), s.end());
-        int ans, lcslen;
-        lcslen = lcs(s, st2, m, m);
-        ans = m - lcslen;
+        int  m = lcs(s, s2, n, n);
+        int ans = n - m;
         return ans;
     }
 };
