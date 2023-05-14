efaba10eb6f413ac21e99b05da5dfa5fffe5b2d2
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-05-14 10:32:34 +0530

    Time: 547 ms (14.88%), Space: 10.8 MB (32.87%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int n;
+    int max_operations;
+    int ans = 0;
+    vector<bool> seen;
+    vector<vector <int>> wish; //store the array of GCDs
+    int maxScore(vector<int>& nums) {
+        n = nums.size();
+        max_operations = n / 2;
+        seen.resize(n, false);
+        vector <int> sum;
+        solve(nums, 0, 0, sum);
+        ans = 0;
+        int interim = 0;
+        for(int i = 0; i < wish.size(); i++)
+        {
+            sort(wish[i].begin(), wish[i].end());
+            
+            for(int j = 0; j < wish[i].size(); j++)
+                interim += (j + 1) * wish[i][j];
+            
+            ans = max(ans, interim);
+            interim = 0;
+        }
+        return ans;
+    }
+    
+    void solve(vector <int>& nums, int interim, int pos, vector <int>& sum)
+    {
+        if(pos == nums.size())
+        {
+            if(interim >= ans)
+            {
+                if(interim > ans) 
+                    wish.clear();
+                wish.push_back(sum); //add the new array sum to wish
+                ans = interim;
+            }
+            return;
+        }
+        if(seen[pos])
+            return solve(nums, interim, pos + 1, sum); 
+        seen[pos] = 1; 
+        for(int a = pos + 1; a < n; a++)   // trying every possibility
+            if(!seen[a])
+            {
+                seen[a] = 1;
+                int x = gcd(nums[pos], nums[a]);
+                sum.push_back(x);   
+                solve(nums, interim + x, pos + 1, sum);   
+                sum.pop_back();
+                seen[a] = 0;
+            }
+        seen[pos] = 0;
+    }
+};
