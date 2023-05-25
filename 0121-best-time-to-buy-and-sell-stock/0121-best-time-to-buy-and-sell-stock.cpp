a0a6b874559bb31da9a93c7ff1d3dbc9e8b5764c
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-05-26 01:52:41 +0530

    Time: 159 ms (14.21%), Space: 93.3 MB (43.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,13 +4,13 @@ public:
         int n = prices.size();
         int buy = prices[0];
         int sell = 0;
-        for(int i=1; i<n; i++)
+        for(int i=0; i<n; i++)
         {
             if(prices[i] < buy)
             {
                 buy = prices[i];
             }
-            sell = max(sell, prices[i] - buy);
+            sell  = max(sell, prices[i] - buy);
         }
         return sell;
     }
