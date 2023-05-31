a5d4eda3a9f1be488e6fc7963e6426f8dd23fec6
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-01 01:08:50 +0530

    Time: 4 ms (42.90%), Space: 5.9 MB (95.70%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    // double recursion(double x, double n)
+    // {
+    //     if(x == 0)
+    //         return 0;
+    //     if(n == 0)
+    //         return 1;
+    //     return x * recursion(x, n-1);
+    // }
     double myPow(double x, int n) {
-        double ans;
-        ans = pow(x,n);
-        return ans;
+         if (n == 0) 
+             return 1.0;
+        if (n % 2 == 0) 
+            return myPow(x * x, n / 2);
+        if (n % 2 == 1) 
+            return x * myPow(x, n - 1);
+        
+        return 1/myPow(x, -n);
     }
 };
