c4f1165384f569239a94b17ae0517e0dc6ade8f7
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-02 00:45:16 +0530

    Time: 21 ms (5.67%), Space: 11.1 MB (82.45%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    class comparator{
-        public:
-        bool operator()(pair<int, int> a, pair<int, int> b)
-        {
-            if(a.first == b.first)
-                return a.second > b.second;
-            return a.first < b.first;
-        }
-    };
     vector<int> frequencySort(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
         unordered_map<int, int> mp;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comparator> minh;
-        
-        for(auto it : nums)
+        for(auto it: nums)
             mp[it]++;
+        sort(nums.begin(), nums.end(), [&](int a, int b)
+             {
+                 return mp[a] != mp[b] ? mp[a] < mp[b] : a>b;
+                 
+             });
+        return nums;
         
-        for(auto it = mp.begin(); it != mp.end(); it++)
-        {
-            minh.push({it->second, it->first});           
-        }
-        while(!minh.empty())
-        {
-            int freq = minh.top().first;
-            int ele = minh.top().second;
-            for(int i=1; i<=freq; i++)
-            {
-                ans.push_back(ele);
-            }
-            minh.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
     }
 };
