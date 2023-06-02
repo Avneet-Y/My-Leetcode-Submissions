cb8b0508eceb79205915f6ccf2eab5860195515d
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-03 02:00:35 +0530

    Time: 15 ms (40.67%), Space: 7.2 MB (91.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,15 +3,38 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.size();
         int n2 = s2.size();
-        sort(s1.begin(), s1.end());
-        for(int i=0; i <= n2-n1; i++)
+        
+        unordered_map<char, int> mp;
+        for(auto it : s1)
+            mp[it]++;
+        
+        int count = mp.size();
+        int i=0; 
+        int j=0;
+        while(j < n2)
         {
-            string temp = s2.substr(i, n1);
-            sort(temp.begin(), temp.end());
-            if(temp == s1)
-                return true;
+            if(mp.find(s2[j]) != mp.end())
+            {
+                mp[s2[j]]--;
+                if(mp[s2[j]] == 0)
+                    count--;
+            }
+            if(j-i+1 < n1)
+                j++;
+            else if( j-i+1 == n1)
+            {
+                if(count == 0)
+                    return true;
+                if(mp.find(s2[i]) != mp.end())
+                {
+                    mp[s2[i]]++;
+                    if(mp[s2[i]] == 1)
+                        count++;
+                }
+                i++;
+                j++;
+            }
         }
         return false;
-        
     }
 };
