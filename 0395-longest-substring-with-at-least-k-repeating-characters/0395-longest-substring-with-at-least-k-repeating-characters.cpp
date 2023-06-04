8627253bf071d1db959aaa276bc096e2bf8253c4
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-05 01:42:36 +0530

    Time: 1832 ms (5.00%), Space: 11.3 MB (36.80%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -2,58 +2,28 @@ class Solution {
 public:
     int longestSubstring(string s, int k) {
         int n = s.size();
-        unordered_map<char,int>mp;
-        // jo bhi char k se kam hai uspe aate hi apni string ka ans break ho jaayega 
-        //eg a a a b b c d d a e a a f f    //a=6, b=2, d=2, c=1, e=1, f=2
-       //    _ _ _ _ _ | _ _ _ | _ _ _ _
-        //            
-        //   |   =  yha ans break hoga
-        //so calc freq of all and then ek pointer chalao jab tak vo invalid freq i.e 
-        //whose freq less than k pe nhi pahunchta tab tak increment kro
-        //jab vo invalid par pahunchega tab fir se usse pehle or uske bad ke pe
-        //apna func chlao aur unme se max apna ans
-         
-        for(int i=0; i<n; i++){
-            mp[s[i]]++;
-        }
-        int l = 0;
-        while(l < n and mp[s[l]] >= k) 
-            l++;
-        if(l == n)  
-            return n;
-        
-
-        int left = longestSubstring(s.substr(0, l), k);
-        int right = longestSubstring(s.substr(l+1), k);
-
-        return max(left,right);
-        
-        
-        /* This is giving TLE
-        
-        int n = s.size();
+        int i=0, j=0;
+        int ans = 0, count = 0;
         unordered_map<char, int> mp;
-        for(int i=0; i<n; i++)
-            mp[s[i]]++;
-        int start = 0;
-        int maxlen = 0;
-        bool valid = true;  //tell there is no valid char whose freq is less than k
-        for(int end = 0; end < n; end++)
+        
+        while(j<n)
         {
-            if(mp[s[end]] < k)
+            mp[s[j]]++;
+            if(mp[s[j]] == k)
+                count++;
+            if(count == mp.size())
             {
-                string temp = s.substr(start, end);
-                maxlen = max(maxlen, longestSubstring(temp, k));
-                start =  end+1;
-                valid = false;
+                ans = max(ans, j-i+1);
             }
-            
+            j++;
+            if(j == n)
+            {
+                mp.clear();
+                i++;
+                j = i;
+                count=0;
+            }  
         }
-        if(valid == true)  // means no char in string where mp[s[end]] < k is met
-            return n;
-        else
-            return max(maxlen, longestSubstring(s.substr(start), k));
-            
-            */
+        return ans;
     }
 };
