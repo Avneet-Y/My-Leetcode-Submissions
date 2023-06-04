3ee1f79ab991d170c2086ac2327074f9dd351581
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-04 19:21:06 +0530

    Time: 468 ms (21.23%), Space: 194 MB (14.47%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,52 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//see compile error soln and prev soln why simple max heap not work priority queue  
-        
-//         int n = nums.size();
-//         vector<int> ans;
-//         priority_queue<pair<int, int>> pq;
-        
-//         int i = 0;
-//         int j = 0;
-//         while(j < n)
-//         {
-//             pq.push({nums[j], j});
-
-//             if(j-i+1 >= k)
-//             {
-//                 while(pq.top().second <= j-k)      // while the topmost element is not present in k elements pop them and move forward
-//                 {
-//                     pq.pop();
-//                     i++;
-//                 }
-//                 ans.push_back(pq.top().first);
-//             }
-//             j++;
-//         }
-//         return ans;
-        
-        
-    priority_queue<pair<int,int>> pq;
-    vector<int> ans;
-    
-    for(int i=0; i<k; i++)
-    {
-        pq.push({nums[i],i});
-    }
-    
-    ans.push_back(pq.top().first);
-    
-    for(int i=k; i<nums.size(); i++)
-    {
-        pq.push({nums[i],i});
+        vector<int> ans;
+        int i=0;int j=0;
+        list<int> l;
+        while(j<nums.size()){
+                 while(l.size()!=0 && l.back()<nums[j]){
+                l.pop_back();
+            }l.push_back(nums[j]);
             
-        while(!pq.empty() && pq.top().second <= i - k)
-            pq.pop();
-        
-        ans.push_back(pq.top().first);
-    }
-    
-    return ans;
+           
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+                ans.push_back(l.front());
+                if(l.front()==nums[i]){
+                    l.pop_front();
+                }i++;j++;
+            }
+        }return ans;
     }
 };
