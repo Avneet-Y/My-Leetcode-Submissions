f08f4ec798bdef2b86113dfcfcff999f99f266b6
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-04 14:20:22 +0530

    Time: 95 ms (75.89%), Space: 41.4 MB (95.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,51 +1,20 @@
-/*
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        int count = 0 ;
-
-         unordered_map<int, int> mp;
-         int pre_sum = 0;
-         mp[pre_sum] = 1;
-         for(int i=0 ;i<n; i++)
-         {
-             pre_sum += nums[i];
-             if(mp.find(pre_sum - k) != mp.end())
-             {
-                count += mp[pre_sum - k];   
-             }
-             mp[pre_sum]++;
-        }
-        return count;
-    }
-};
-
-*/
-
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
+        int len = 0;
+        int sum = 0;
         unordered_map<int, int> mp;
-        int pre_sum = 0;
-        
-        for(int i=0 ;i<n; i++)
+        mp[sum] = 1;
+        for(int i=0; i<n; i++)
         {
-            pre_sum += nums[i];
-            if(pre_sum == k)
-                count++;
-            
-            if(mp.find(pre_sum - k) != mp.end())
+            sum += nums[i];
+            if(mp.find(sum - k) != mp.end())
             {
-                count += mp[pre_sum - k];
+                len += mp[sum-k];
             }
-            mp[pre_sum]++;
+            mp[sum]++;
         }
-        return count;
-         
+        return len;  
     }
 };
-
