41b3e75770602b4b700751a6d76144540e635355
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-09 18:54:52 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.cpp
+++ Bitonic Point - GFG/bitonic-point.cpp
@@ -8,44 +8,42 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	
-	//this is a question of Peak Element
-	
-	int solve(int arr[], int n, int start, int end)
+	int binarypeak(int arr[], int n)
 	{
-	    while(start <= end)
-	    {
-	        int mid = start + (end-start)/2;
-	        if(mid > 0 && mid < n-1)
-	        {
-	            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
-	                return arr[mid];
-	            else if(arr[mid-1] > arr[mid])
-	                end = mid - 1;
-	            else
-	                start = mid + 1;
-	        }
-	        else if(mid == 0)
-	        {
-	            if(arr[0] > arr[1])
-	                return arr[0];
-	            else
-	                return arr[1];
-	        }
-	        else if(mid == n-1)
-	        {
-	            if(arr[n-1] > arr[n-2])
-	                return arr[n-1];
-	            else
-	                return arr[n-2];
-	        }
-	    }
-	    return arr[start];
-	    
+	    int start = 0;
+        int end = n-1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(mid > 0 && mid < n-1)
+            {
+                if(arr[mid] >= arr[mid+1] && arr[mid] >= arr[mid-1])
+                    return arr[mid];
+                else if(arr[mid+1] > arr[mid])
+                    start = mid+1;
+                else if(arr[mid-1] > arr[mid])
+                    end = mid-1;      
+            }
+            else if(mid == 0)
+            {
+                if(arr[0] > arr[1])
+                    return arr[0];
+                else                         
+                    return arr[1];
+            }
+            else if(mid == n-1)
+            {
+                if(arr[n-1] > arr[n-2])
+                    return arr[n-1];
+                else
+                    return arr[n-2];
+            }
+        }
+        return arr[0];
 	}
 	int findMaximum(int arr[], int n) {
 	    // code here
-	    int ans = solve(arr, n, 0, n-1);
+	    int ans = binarypeak(arr, n);
 	    return ans;
 	}
 };
