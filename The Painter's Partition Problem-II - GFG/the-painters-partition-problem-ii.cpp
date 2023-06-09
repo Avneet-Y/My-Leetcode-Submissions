feccfaa1864783de08c1748201cfa5a16dfc6d46
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-10 00:13:53 +0530

    Added solution - LeetHub

--- The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
+++ The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
@@ -11,54 +11,49 @@ using namespace std;
 class Solution
 {
   public:
-  
-    bool isvalid(int arr[], int n, int k, int mid)
+   bool isvalid(int arr[], int n, int k, int mx)
     {
-        long long sum = 0, paint = 1;
+        int painter = 1;
+        int sum = 0;
         for(int i=0; i<n; i++)
         {
             sum += arr[i];
-            if(sum > mid)
+            if(sum > mx)
             {
-                paint++;
+                painter++;
                 sum = arr[i];
             }
-            if(paint > k)
+            if(painter > k)
                 return false;
         }
         return true;
     }
-  
-    long long solve(int arr[], int n, int k )
+    long long minTime(int arr[], int n, int k)
     {
-        int res = -1;
-        int maxi = *max_element(arr, arr + n);
-        int start = maxi;
+        // code here
+        // return minimum time
+        long long ans = -1;
         long long sum = 0;
+        int maxi = INT_MIN;
+        // if(n < k)
+        //     return -1;
         for(int i=0; i<n; i++)
         {
             sum += arr[i];
+            maxi = max(arr[i], maxi);
         }
+        int start = maxi;
         long long end = sum;
         while(start <= end)
         {
-            int mid = start + (end-start)/2;
-            if(isvalid(arr, n, k, mid) == true)
-            {
-                res = mid;
+            long long mid = start + (end-start)/2;
+            if(isvalid(arr, n, k, mid) == true){
+                ans = mid;
                 end = mid-1;
             }
             else
                 start = mid+1;
         }
-        return res;
-    }
-    
-    long long minTime(int arr[], int n, int k)
-    {
-        // code here
-        // return minimum time
-        long long ans = solve(arr, n, k);
         return ans;
     }
 };
