5ffa67414e4c9cef8f72e7ffb09ba0f6762721ba
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-09 15:51:14 +0530

    Time: 4 ms (76.50%), Space: 8.9 MB (60.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int start, int end, int size)
+    int binarypeak(vector<int>& nums, int n)
     {
+        int start = 0;
+        int end = n-1;
+        
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            if(start == end)            //if size=1
-                return start;
-            if(mid > 0 && mid < size-1)
+            if(mid > 0 && mid < n-1)
             {
-                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
-                {
+                if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])
                     return mid;
-                }
-                else if(nums[mid-1] > nums[mid])  //which is bigger go in that direction
+                else if(nums[mid] < nums[mid-1])
                     end = mid-1;
-                else
+                else if(nums[mid+1] > nums[mid])
                     start = mid+1;
             }
-            //to check last two elements edge
             else if(mid == 0)
             {
-                if(nums[0]>nums[1])
+                if(nums[0] > nums[1])
                     return 0;
-                else 
+                else                         
                     return 1;
             }
-            else if(mid == size-1)
+            else if(mid == n-1)
             {
-                if(nums[size-1] > nums[size-2])
-                    return size-1;
+                if(nums[n-1] > nums[n-2])
+                    return n-1;
                 else
-                    return size-2;
-            }    
+                    return n-2;
+            }
         }
-        return start;
+        return nums[0];
     }
     
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int ans = solve(nums, 0, n-1, n);
+        if(n == 1)
+            return 0;
+        int ans = binarypeak(nums, n);
         return ans;
+        
+        
     }
 };
