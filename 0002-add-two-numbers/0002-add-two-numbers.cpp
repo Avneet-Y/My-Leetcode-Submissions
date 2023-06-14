6d82d69737dc37d7706d5275301bf2bc9f69c40d
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-06-15 01:29:10 +0530

    Time: 48 ms (23.83%), Space: 71.4 MB (32.68%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,73 +10,55 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head)
-    {
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
-        while(curr != NULL)
-        {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+
+     void insertattail(ListNode* &head, ListNode* &tail, int val)
+     {
+         ListNode* temp = new ListNode(val);
+         if(head == NULL)
+         {
+             head = temp;
+             tail = temp;
+             return;
+         }
+         else
+         {
+             tail->next  = temp;
+             tail = temp;
+         }
+     }
     
-    void insertattail(ListNode* &head, ListNode* &tail, int digit)
-    {
-        ListNode* temp = new ListNode(digit);
-        if(head == NULL)
-        {
-            head = temp;
-            tail = temp;
-            return;
-        }
-        else
-        {
-            tail->next = temp;
-            tail = temp;
-        }
-    }
-    
-    ListNode* add(ListNode* l1, ListNode* l2)
-    {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
         int carry = 0;
         ListNode* anshead = NULL;
         ListNode* anstail = NULL;
         
         while(l1 != NULL || l2 != NULL || carry != 0)
         {
-            int value1 = 0;
+            int val1 = 0, val2 = 0;
             if(l1 != NULL)
-                 value1 = l1->val;
+            {
+                val1 = l1->val;
+            }
             
-            int value2 = 0;
             if(l2 != NULL)
-                 value2 = l2->val;
+            {
+                val2 = l2->val;
+            }
             
-            int sum = carry + value1 + value2;
-            int digit = sum % 10;
+            int sum = carry + val1 + val2;
+            int digit = sum%10;
             insertattail(anshead, anstail, digit);
             carry = sum/10;
             if(l1 != NULL)
+            {
                 l1 = l1->next;
+            }
             if(l2 != NULL)
+            {
                 l2 = l2->next;
+            }
         }
         return anshead;
-    }
-    
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        ListNode* ans = add(l1, l2);
-        l1 = reverse(l1);
-        l2 = reverse(l2);
-        //ans = reverse(ans);
-        
-        return ans;
-        
     }
 };
