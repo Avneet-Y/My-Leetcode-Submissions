247f8fad9d1bcee2844c784c5aec88cbb6879514
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-07 01:37:47 +0530

    Time: 118 ms (10.23%), Space: 56.8 MB (59.05%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -19,6 +19,8 @@ public:
         TreeNode *curr = root;
         while(true)
         {
+            if(curr->val == val)
+                return root;
             if(curr->val <= val)
             {
                 if(curr->right != NULL)
