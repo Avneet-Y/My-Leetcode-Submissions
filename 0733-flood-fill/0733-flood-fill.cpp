9ea0fd04a1a230cd9665ed7997377ea536259556
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-15 01:17:34 +0530

    Time: 14 ms (29.20%), Space: 13.8 MB (98.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,76 +1,36 @@
-/*
-
 class Solution {
 public:
-    bool isvalid(int i, int j, int n, int m, vector<vector<int>>& vec, int color, int oldcolor)
+    bool isvalid(vector<vector<int>>& vec, int i, int j, int n, int m, int oldcolor, int newcolor)
     {
-        if(i >= 0 and i < n and j >= 0 and j < m and vec[i][j] == oldcolor && vec[i][j] != color)
+        if(i>=0 && i<n && j>=0 && j<m && vec[i][j] == oldcolor && vec[i][j] != newcolor)
             return true;
-        else
-            return false;
+        return false;
+        
     }
-    
-    void dfs(int i, int j, int n, int m, vector<vector<int>>& vec, int color, int oldcolor)
+    void dfs(vector<vector<int>>& vec, int i, int j, int n, int m, int oldcolor, int newcolor)
     {
-        vec[i][j] = color;
+        vec[i][j] = newcolor;
         
-        if (isvalid(i+1, j, n, m, vec, color, oldcolor))
-            dfs(i+1, j, n, m, vec, color, oldcolor);
+        if(isvalid(vec, i+1, j, n, m, oldcolor, newcolor))
+            dfs(vec, i+1, j, n, m, oldcolor, newcolor);
         
-        if (isvalid(i, j+1, n, m, vec, color, oldcolor))
-            dfs(i, j+1, n, m, vec, color, oldcolor);
+        if(isvalid(vec, i-1, j, n, m, oldcolor, newcolor))
+            dfs(vec, i-1, j, n, m, oldcolor, newcolor);
         
-        if (isvalid(i-1, j, n, m, vec, color, oldcolor))
-            dfs(i-1, j, n, m, vec, color, oldcolor);
+        if(isvalid(vec, i, j+1, n, m, oldcolor, newcolor))
+            dfs(vec, i, j+1, n, m, oldcolor, newcolor);
         
-        if (isvalid(i, j-1, n, m, vec, color, oldcolor))
-            dfs(i, j-1, n, m, vec, color, oldcolor);
-        }
-    
+        if(isvalid(vec, i, j-1, n, m, oldcolor, newcolor))
+            dfs(vec, i, j-1, n, m, oldcolor, newcolor);
+        
+        
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
         int n = image.size();
         int m = image[0].size();
         vector<vector<int>> vec = image;
         int oldcolor = vec[sr][sc];
-        dfs(sr, sc, n, m, vec, color, oldcolor);
-        return vec;
-    }
-};
-
-*/
-//Another way for dfs
-
-class Solution {
-public:
-    
-    void dfs(int row, int col, vector<vector<int>>& vec, int color, int oldcolor, int delrow[], int delcol[])
-    {
-        vec[row][col] = color;
-        int n = vec.size();
-        int m = vec[0].size();
-        for(int i=0; i<4; i++)
-        {
-            int nrow = delrow[i] + row;
-            int ncol = delcol[i] + col;
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-               vec[nrow][ncol] == oldcolor && vec[nrow][ncol] != color)
-            {
-                dfs(nrow, ncol, vec, color, oldcolor, delrow, delcol);
-            }
-        }
-    }
-    
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>> vec = image;
-        int oldcolor = vec[sr][sc];
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        dfs(sr, sc, vec, color, oldcolor, delrow, delcol);
+        dfs(vec, sr, sc, n, m, oldcolor, color);
         return vec;
     }
 };
