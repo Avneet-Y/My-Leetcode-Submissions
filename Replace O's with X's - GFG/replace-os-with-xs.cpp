960000a6966db934d64cd8a4b7486e2654f229ae
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-16 19:15:19 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -9,64 +9,57 @@ using namespace std;
 
 class Solution{
 public:
-void dfs(int row, int col, vector<vector<int>>& visited, vector<vector<char>>& board, int delrow[], int delcol[])
+void dfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>>& mat, int delrow[], int delcol[])
     {
-        int n = board.size();
-        int m = board[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
         visited[row][col] = 1;
         
         for(int i=0; i<4; i++)
         {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow >=0 && nrow<n && ncol >=0 && ncol < m && visited[nrow][ncol] == 0 && board[nrow][ncol] == 'O')
+            int nrow = delrow[i] + row;
+            int ncol = delcol[i] + col;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol] == 0 && mat[nrow][ncol] == 'O')
             {
-                dfs(nrow, ncol, visited, board, delrow, delcol);
+                dfs(nrow, ncol, visited, mat, delrow, delcol);
             }
         }
     }
-    
-    
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
         // code here
+        
         vector<vector<int>> visited(n, vector<int>(m, 0));
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
         
-        for(int j = 0; j < m; j++)
-        {
-            if(!visited[0][j] && mat[0][j] == 'O')  //1st row
-            {
-                dfs(0, j, visited, mat, delrow, delcol);
-            }
-            if(!visited[n-1][j] && mat[n-1][j] == 'O')  //last row
-            {
-                dfs(n-1, j, visited, mat, delrow, delcol);
-            }
-        }
+        int delrow[] = {1, -1, 0, 0};
+        int delcol[] = {0, 0, -1, 1};
         
-        for(int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
         {
-            if(!visited[i][0] && mat[i][0] == 'O')  //1st col
-            {
+            if(visited[i][0] == 0 && mat[i][0] == 'O')
                 dfs(i, 0, visited, mat, delrow, delcol);
-            }
-            if(!visited[i][m-1] && mat[i][m-1] == 'O')  //last col
-            {
-                dfs(i, m-1, visited, mat, delrow, delcol);
-            }
+            if(visited[i][m-1] == 0 && mat[i][m-1] == 'O')
+                dfs(i, m-1, visited, mat, delrow, delcol);   
         }
-        for(int i = 0; i < n; i++)
+        
+        for(int j=0; j<m; j++)
         {
-            for(int j = 0; j < m; j++)
+            if(visited[0][j] == 0 && mat[0][j] == 'O')
+                dfs(0, j, visited, mat, delrow, delcol);
+            if(visited[n-1][j] == 0 && mat[n-1][j] == 'O')
+                dfs(n-1, j, visited, mat, delrow, delcol);
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
             {
-                if(!visited[i][j] && mat[i][j] == 'O')
+                if(mat[i][j] == 'O' && visited[i][j] == 0)
                 {
                     mat[i][j] = 'X';
                 }
             }
-        } 
+        }
         return mat;
     }
 };
