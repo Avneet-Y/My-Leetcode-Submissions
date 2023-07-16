e17c4e6d4a771c306631acd2477c22eb89d92199
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-16 20:48:20 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,61 +10,56 @@ using namespace std;
 
 class Solution {
   public:
-  void dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &visited, int delrow[], int delcol[] )
-  {
-      int n = grid.size();
-      int m = grid[0].size();
-      visited[row][col] = 1;
-      for(int i = 0; i<4; i++)
-      {
-          int nrow = row + delrow[i];
-          int ncol = col + delcol[i];
-          if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1)
-          {
-              dfs(nrow, ncol, grid, visited, delrow, delcol);
-          }
-      }
-  }
+  void dfs(int row, int col, vector<vector<int>> &visited, vector<vector<int>>& grid, int delrow[], int delcol[])
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        visited[row][col] = 1;
+        
+        for(int i=0; i<4; i++)
+        {
+            int nrow = delrow[i] + row;
+            int ncol = delcol[i] + col;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1)
+            {
+                dfs(nrow, ncol, visited, grid, delrow, delcol);
+            }
+        }
+    }
     
     int numberOfEnclaves(vector<vector<int>> &grid) {
         // Code here
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> visited(n, vector<int>(m, 0));
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
         int count = 0;
         
-        for(int j=0; j<m; j++)   
+        int delrow[] = {1, -1, 0, 0};
+        int delcol[] = {0, 0, -1, 1};
+        
+        for(int i=0; i<n; i++)
         {
-            if(grid[0][j] == 1 && !visited[0][j])   //first row
-            {
-                dfs(0, j, grid, visited, delrow, delcol);
-            }
-            if(grid[n-1][j] == 1 && !visited[n-1][j])  //last row
-            {
-                dfs(n-1, j, grid, visited, delrow, delcol);
-            }
+            if(visited[i][0] == 0 && grid[i][0] == 1)
+                dfs(i, 0, visited, grid, delrow, delcol);
+            if(visited[i][m-1] == 0 && grid[i][m-1] == 1)
+                dfs(i, m-1, visited, grid, delrow, delcol);   
         }
         
-        for(int i=0; i<n; i++)   
+        for(int j=0; j<m; j++)
         {
-            if(grid[i][0] == 1 && !visited[i][0])   //first col
-            {
-                dfs(i, 0, grid, visited, delrow, delcol);
-            }
-            if(grid[i][m-1] == 1 && !visited[i][m-1])  //last col
-            {
-                dfs(i, m-1, grid, visited, delrow, delcol);
-            }
+            if(visited[0][j] == 0 && grid[0][j] == 1)
+                dfs(0, j, visited, grid, delrow, delcol);
+            if(visited[n-1][j] == 0 && grid[n-1][j] == 1)
+                dfs(n-1, j, visited, grid, delrow, delcol);
         }
+        
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(grid[i][j] == 1 and visited[i][j] == 0)
+                if(grid[i][j] == 1 && visited[i][j] == 0)
                 {
-                    grid[i][j] = 0;
+                    // grid[i][j] = 1;
                     count++;
                 }
             }
