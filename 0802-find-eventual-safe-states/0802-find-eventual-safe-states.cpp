ef839312b437f704f4e718b3a5e4b309f9e12e5a
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-17 23:43:17 +0530

    Time: 162 ms (88.98%), Space: 48.3 MB (63.37%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    bool dfscheck(int node, vector<vector<int>>& graph, vector<int> &visited, vector<int> & pathvisited, vector<int> &check)
-  {
-      visited[node] = 1;
-      pathvisited[node] = 1;
-      check[node] = 0;
-      
-      for(auto it : graph[node])
-      {
-          if(visited[it] == 0)
-          {
-              if(dfscheck(it, graph, visited, pathvisited, check) == true)
-              {
-                  check[node] = 0;
-                  return true;
-              }
-          }
-          else if(pathvisited[it] == 1)
-          {
-                  check[node] = 0;
-                  return true;
-          }
-      }
-      
-      check[node] = 1;
-      pathvisited[node] = 0;
-      return false;
-  }
+    bool dfs(int node, vector<int> &visited, vector<int> &samepath, vector<vector<int>>& graph, vector<int> &check)
+    {
+        visited[node] = 1;
+        samepath[node] = 1;
+        check[node] = 0;
+        for(auto it : graph[node])
+        {
+            if(visited[it] == 0)
+            {
+                if(dfs(it, visited, samepath, graph, check) == true)
+                {
+                    check[node] = 0;
+                    return true;
+                }
+            }
+            else if(samepath[it] == 1)
+            {
+                check[node] = 0;
+                return true;
+            }
+        }
+        check[node] = 1;
+        samepath[node] = 0;
+        return false;
+    }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        
         vector<int> visited(n, 0);
-        vector<int> pathvisited(n, 0);
+        vector<int> samepath(n, 0);
         vector<int> check(n, 0);
         
         vector<int> safenode;
         
         for(int i=0; i<n; i++)
         {
-            if(visited[i] == 0)
-            {
-                dfscheck(i, graph, visited, pathvisited, check);
-            }
+            dfs(i, visited, samepath, graph, check);
         }
         for(int i=0; i<n; i++)
         {
             if(check[i] == 1)
-            {
                 safenode.push_back(i);
-            }
         }
+        
         return safenode;
+        
     }
 };
