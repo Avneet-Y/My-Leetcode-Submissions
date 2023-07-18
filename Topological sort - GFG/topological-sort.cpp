550cc568085cd3b273d63f5beef82e353ae31b7e
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-19 01:03:02 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -3,86 +3,41 @@
 using namespace std;
 
 // } Driver Code Ends
-
-/*
 class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	void dfs(int node, int visited[], vector<int> adj[], stack<int>& st)
+	void dfs(int node, vector<int> adj[], int visited[], stack<int> &st)
 	{
 	    visited[node] = 1;
 	    for(auto it : adj[node])
 	    {
-	        if(visited[it] == 0)
+	        if(!visited[it])
 	        {
-	            dfs(it, visited, adj, st);
+	            dfs(it, adj, visited, st);
 	        }
 	    }
 	    st.push(node);
 	}
+	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+	    vector<int> ans;
 	    int visited[V] = {0};
+	    
 	    stack<int> st;
 	    for(int i=0; i<V; i++)
 	    {
-	        if(visited[i] == 0)
+	        if(visited[i] != 1)
 	        {
-	            dfs(i, visited, adj, st);
+	            dfs(i, adj, visited, st);
 	        }
 	    }
-	    vector<int> ans;
 	    while(!st.empty())
 	    {
 	        ans.push_back(st.top());
 	        st.pop();
-	    }
-	    return ans;
-
-	}
-};
-*/
-class Solution
-{
-    public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    int indexdegree[V] = {0};
-	    queue<int> q;
-	    for(int i=0; i<V; i++)
-	    {
-	        for(auto it : adj[i])
-	        {
-	            indexdegree[it]++;
-	        }
-	    }
-	    for(int i=0; i<V; i++)
-	    {
-	        if(indexdegree[i] == 0)
-	        {
-	            q.push(i);
-	        }
-	    }
-	  
-	    vector<int> ans;
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        
-	        for(auto it : adj[node])
-	        {
-	            indexdegree[it]--;
-	            if(indexdegree[it] == 0)
-	            {
-	                q.push(it);
-	            }
-	        }
 	    }
 	    return ans;
 	}
