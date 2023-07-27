6c29db19af2d4807a9894734ffc201bc107b3ed4
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-07-28 01:44:20 +0530

    Time: 111 ms (13.15%), Space: 30.6 MB (17.81%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        vector<vector<int>> ans;
-		map<int,vector<vector<int>>> mp;
+//         vector<vector<int>> ans;
+// 		map<int,vector<vector<int>>> mp;
         
-		for(int i=0; i<rows; i++)
+// 		for(int i=0; i<rows; i++)
+//         {
+// 			for(int j=0; j<cols; j++)
+//             {
+// 				int dis = abs(i - rCenter) + abs(j - cCenter);
+// 				mp[dis].push_back({i,j});
+// 			}
+// 		}
+// 		for(auto i : mp)
+//         {
+// 			for(auto j : i.second) 
+//                 ans.push_back(j);
+// 		}
+// 		return ans;
+        
+        vector<vector<int>> ans;
+        priority_queue<pair<int,vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int,vector<int>>> > pq;
+        for(int i = 0; i<rows; i++)
         {
-			for(int j=0; j<cols; j++)
+            for(int j = 0; j<cols; j++)
             {
-				int dis = abs(i - rCenter) + abs(j - cCenter);
-				mp[dis].push_back({i,j});
-			}
-		}
-		for(auto i : mp)
+                int dis = abs(rCenter - i) + abs(cCenter - j);
+                vector<int> mat;
+                mat.push_back(i);
+                mat.push_back(j);
+                pq.push({dis, mat});
+            }
+        }
+        while(pq.size() > 0)
         {
-			for(auto j : i.second) 
-                ans.push_back(j);
-		}
-		return ans;
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
     }
 };
