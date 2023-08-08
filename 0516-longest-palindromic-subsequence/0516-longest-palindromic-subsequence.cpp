82fdd983f0b72a3acf7c0cdac21144975626ec04
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-08-09 01:24:05 +0530

    Time: 45 ms (98.65%), Space: 10.9 MB (72.98%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int lps(string s1, string s2, int n)
+    int lcs(string s, string s2, int n)
     {
+        int dp[n+1][n+1];
         for(int i=0; i<n+1; i++)
         {
             for(int j=0; j<n+1; j++)
             {
                 if(i == 0 || j == 0)
+                {
                     dp[i][j] = 0;
+                }
             }
         }
         for(int i=1; i<n+1; i++)
         {
             for(int j=1; j<n+1; j++)
             {
-                if(s1[i-1] == s2[j-1])
+                if(s[i-1] == s2[j-1])
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else
+                {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                
+                }
             }
         }
         return dp[n][n];
@@ -31,6 +34,9 @@ public:
         int n = s.size();
         string s2 = s;
         reverse(s2.begin(), s2.end());
-        return lps(s, s2, n);
+        
+        
+        int ans = lcs(s, s2, n);
+        return ans;
     }
 };
