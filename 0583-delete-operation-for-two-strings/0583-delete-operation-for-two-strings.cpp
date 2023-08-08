46f86d32c4cf170eadacdf0f63ef1f3d84d02a7c
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-08-09 01:27:36 +0530

    Time: 3 ms (99.88%), Space: 7.6 MB (77.43%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    
-    int lcs(string w1, string w2, int m, int n)
+    int lcs(string word1, string word2, int n1, int n2)
     {
-        int dp[m+1][n+1];
-        for(int i = 0; i < m+1; i++)
+        int dp[n1+1][n2+1];
+        for(int i=0; i<n1+1; i++)
         {
-            for(int j = 0; j < n+1; j++)
+            for(int j=0; j<n2+1; j++)
             {
                 if(i == 0 || j == 0)
+                {
                     dp[i][j] = 0;
+                }
             }
-            
         }
-        for(int i = 1; i < m+1; i++)
+        for(int i=1; i<n1+1; i++)
         {
-            for(int j = 1; j < n+1; j++)
+            for(int j=1; j<n2+1; j++)
             {
-                if(w1[i-1] == w2[j-1])
+                if(word1[i-1] == word2[j-1])
+                {
                     dp[i][j] = 1 + dp[i-1][j-1];
+                }
                 else
+                {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n1][n2];
     }
     
     int minDistance(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
         
-        int len1 = word1.size();
-        int len2 = word2.size();
-        int ans = lcs(word1, word2, len1, len2);
-        return (len1 - ans) + (len2 - ans);
+        int ans = lcs(word1, word2, n1, n2);
+        return (n1-ans) + (n2-ans);
         
     }
 };
