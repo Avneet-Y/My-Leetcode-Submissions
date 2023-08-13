a4ec09cff633d2ab07df4f535bbb0d40fb10d666
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-08-14 01:54:03 +0530

    Time: 6 ms (71.97%), Space: 7.8 MB (57.99%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,64 +1,34 @@
-/*
-
 class Solution {
 public:
+    bool vowel(char c)
+    {
+        char ch = tolower(c);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        return false; 
+    }
+    
     string reverseVowels(string s) {
-        
+        int n = s.size();
         int i = 0;
-        int j = s.size() - 1;
-        
-        while(i < j)
+        int j = n-1;
+        while(i <= j)
         {
-            while(i<j)
+            if(vowel(s[i]) == true && vowel(s[j]) == true)
             {
-                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-                    break;
-                if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
-                    break;
+                swap(s[i], s[j]);
                 i++;
-            }
-            while(i < j)
-            {
-                if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
-                    break;
-                if(s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U')
-                    break; 
                 j--;
             }
-            swap(s[i], s[j]);
-            i++;
-            j--;
+            else if(vowel(s[i]) == false)
+            {
+                i++;
+            }
+            else if(vowel(s[j]) == false)
+            {
+                j--;
+            }
         }
         return s;
-    }
-};
-
-*/
-
-
-class Solution {
-public:
-    string reverseVowels(string s) {
-        
-        int i = 0;
-        int j = s.size() - 1;
-        unordered_set<char> st;
-        st.insert({'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'});
-        while(i < j)
-        {
-            while(!st.count(s[i]) && i < j)
-            {
-                i++;       
-            }
-            while(!st.count(s[j]) && i < j)
-            {
-                j--;       
-            }
-               swap(s[i], s[j]);
-               i++;
-               j--;
-          
-        }
-               return s;
     }
 };
