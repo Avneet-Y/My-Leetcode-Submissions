8bba513834a9aed1cbc120c3f102a33c900f0230
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-08-26 00:06:14 +0530

    Time: 13 ms (59.94%), Space: 13.3 MB (52.60%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,8 +3,34 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+//         unordered_map<int, bool> rowmp;
+//         unordered_map<int, bool> colmp;
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<m; j++)
+//             {
+//                 if(matrix[i][j] == 0)
+//                 {
+//                     rowmp[i] = true;
+//                     colmp[j] = true;
+//                 }
+//             }
+//         }
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<m; j++)
+//             {
+//                 if(rowmp[i] == true || colmp[j] == true)
+//                 {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
         bool row1 = false;
         bool col1 = false;
+        
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
@@ -19,11 +45,12 @@ public:
                     {
                         col1 = true;
                     }
-                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
+        
         for(int i=1; i<n; i++)
         {
             for(int j=1; j<m; j++)
@@ -37,16 +64,14 @@ public:
         if(col1 == true)
         {
             for(int i=0; i<n; i++)
-            {
                 matrix[i][0] = 0;
-            }
         }
         if(row1 == true)
         {
             for(int j=0; j<m; j++)
-            {
                 matrix[0][j] = 0;
-            }
         }
+        
+        
     }
 };
