8db7b6fbea30e90992e65d76327f5d00757f8cfa
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-09-01 20:26:56 +0530

    Time: 2 ms (90.66%), Space: 7.2 MB (58.67%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,60 +1,59 @@
 class Solution {
 public:
-//     bool issafe(int row, int col, int n, vector<string> &boards)
-//     {
-//         int temprow = row;
-//         int tempcol = col;
+    bool issafe(int row, int col, int n, vector<string> &boards)
+    {
+        int temprow = row;
+        int tempcol = col;
         
-//         while(row >= 0 && col >= 0)
-//         {
-//             if(boards[row][col] == 'Q')
-//                 return false;
-//             row--;
-//             col--;
-//         }
-//         row = temprow;
-//         col = tempcol;
+        while(row >= 0 && col >= 0)
+        {
+            if(boards[row][col] == 'Q')
+            {
+                return false;
+            }
+            row--;
+            col--;
+        }
+        col = tempcol;
+        row = temprow;
         
-//         while(col >= 0)
-//         {
-//             if(boards[row][col] == 'Q')
-//                 return false;
-//             col--;
-//         }
-//         row = temprow;
-//         col = tempcol;
+        while(col >= 0)
+        {
+            if(boards[row][col] == 'Q')
+            {
+                return false;
+            }
+            col--;
+        }
+        col = tempcol;
+        row = temprow;
         
-//         while(row < n && col >= 0)
-//         {
-//             if(boards[row][col] == 'Q')
-//                 return false;
-//             row++;
-//             col--;
-//         }
-//         return true;   
-//     }
+        while(row < n && col >= 0)
+        {
+            if(boards[row][col] == 'Q')
+            {
+                return false;
+            }
+            row++;
+            col--;
+        }
+        return true;
+    }
     
-    void solve(int col, vector<vector<string>> &ans, vector<string> &boards, int n, vector<int> &leftrow, vector<int> &upperdiag, vector<int> &lowerdiag)
+    void solve(int n, vector<vector<string>> &ans, vector<string> &boards, int col)
     {
         if(col == n)
         {
             ans.push_back(boards);
             return;
         }
-        
-        for(int row = 0; row<n; row++)
+        for(int row=0; row<n; row++)
         {
-            if(leftrow[row] == 0 && upperdiag[n-1+col-row] == 0 && lowerdiag[row+col]== 0)
+            if(issafe(row, col, n, boards) == true)
             {
                 boards[row][col] = 'Q';
-                leftrow[row] = 1;
-                upperdiag[n-1+col-row] = 1;
-                lowerdiag[row+col] = 1;
-                solve(col+1, ans, boards, n, leftrow, upperdiag, lowerdiag);
+                solve(n, ans, boards, col+1);
                 boards[row][col] = '.';
-                leftrow[row] = 0;
-                upperdiag[n-1 + col-row] = 0;
-                lowerdiag[row + col] = 0;
             }
         }
     }
@@ -66,10 +65,7 @@ public:
         {
             boards[i] = s;
         }
-        vector<int> leftrow(n, 0); 
-        vector<int> upperdiag(2*n - 1, 0);
-        vector<int> lowerdiag(2*n - 1, 0);
-        solve(0, ans, boards, n, leftrow, upperdiag, lowerdiag);
-        return ans;
+        solve(n, ans, boards, 0);
+        return ans;     
     }
 };
