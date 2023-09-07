fd486c71e8065eeb6ce6bba48941529dd3bae525
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-09-07 19:05:25 +0530

    Time: 57 ms (88.34%), Space: 11.1 MB (91.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        
-        int n = coins.size();
+    int solve(vector<int>& coins, int amount, int n)
+    {
         long long dp[n+1][amount+1];
-        for(int i=0; i<=n; i++)
+        for(int i=0; i<n+1; i++)
         {
-                    dp[i][0] = 0;
+            dp[i][0] = 0;
         }
-        for(int j=1; j<=amount; j++)
+        for(int j=1; j<amount+1; j++)
         {
             dp[0][j] = INT_MAX;
         }
-        for(int i=1; i<=n; i++)
+        for(int i=1; i<n+1; i++)
         {
-            for(int j=1; j<=amount; j++)
+            for(int j=1; j<amount+1; j++)
             {
                 if(coins[i-1] <= j)
+                {
                     dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                }
                 else
                     dp[i][j] = dp[i-1][j];
             }
         }
         if (dp[n][amount] == INT_MAX) 
             return -1;
-        return dp[n][amount]; 
+        return dp[n][amount];
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int ans = solve(coins, amount, n);
+        return ans;
         
     }
 };
