e705dd02a8f6e112356e2a5f449dcebdc615bf30
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-10-09 01:50:36 +0530

    Time: 196 ms (5.01%), Space: 104.5 MB (25.23%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int,int>> st;
-        vector<int> ans;
-        int n = temperatures.size();
-        for(int i = n-1; i >= 0; i--)
-        {
-            if(st.size() == 0)
-            {
-                ans.push_back(0);
-            }
-            else if(st.size() > 0 && st.top().first > temperatures[i])
-            {
-                ans.push_back(st.top().second - i);
-            }
-            else if(st.size() > 0 && st.top().first <= temperatures[i])
-            {
-                while(st.size() > 0 && st.top().first <= temperatures[i])
-                {
-                    st.pop();
-                }
-                if(st.size() == 0)
-                {
-                    ans.push_back(0);
-                }
-                else
-                {
-                    ans.push_back(st.top().second-i);
-                }
-            }
-            st.push({temperatures[i],i});
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+       
+       int n = temperatures.size();
+       map<int, int> mp;
+       stack<pair<int, int>> st;
+       vector<int> temp;
+       for(int i=n-1; i>=0; i--)
+       {
+           if(st.size() == 0)
+                temp.push_back(0);
+           else if(st.size() > 0 && st.top().first > temperatures[i])
+           {
+               temp.push_back(st.top().second - i);
+           }
+           else if(st.size() > 0 && st.top().first <= temperatures[i])
+           {
+               while(st.size() > 0 && st.top().first <= temperatures[i])
+               {
+                   st.pop();
+               }
+               if(st.size() == 0)
+                    temp.push_back(0);
+               else 
+                    temp.push_back(st.top().second - i);
+           }
+           st.push({temperatures[i], i});
+       }
+       reverse(temp.begin(), temp.end());
+       return temp;
     }
 };
