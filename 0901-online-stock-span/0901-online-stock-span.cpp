8e7941215455384b6c14a1b28e3963be249eb2ee
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-10-09 01:39:13 +0530

    Time: 148 ms (99.60%), Space: 84.5 MB (41.68%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,78 +1,11 @@
-// class StockSpanner {
-// public:
-//      stack<int> st;
-//      vector<int> vec;
-//      int i=0;
-    
-//     StockSpanner() {
-        
-//     }
-    
-//     int next(int price) 
-//     {
-//         vec.push_back(price);
-//         int span;
-        
-//             while(st.empty()==false && vec[st.top()] <= price)
-//                 st.pop();
-//             if(st.empty())
-//                 span = i+1;
-//             else
-//                 span = i - st.top(); 
-            
-//             st.push(i++);
-        
-//         return span;
-        
-//     }
-// };
-
-// /**
-//  * Your StockSpanner object will be instantiated and called as such:
-//  * StockSpanner* obj = new StockSpanner();
-//  * int param_1 = obj->next(price);
-//  */
-
-
-
-// class StockSpanner {
-// public:
-//      stack<int> st;
-    
-//     StockSpanner() {
-        
-//     }
-    
-//     int next(int price) 
-//     {
-//         int span;
-        
-//             while(st.empty()==false && vec[st.top()] <= price)
-//                 st.pop();
-//             if(st.empty())
-//                 span = i+1;
-//             else
-//                 span = i - st.top(); 
-            
-//             st.push(i++);
-        
-//         return span;
-        
-//     }
-// };
-
-
-
 class StockSpanner {
 public:
-     stack<pair<int, int>> st;
-    
+    stack<pair<int, int>> st;
     StockSpanner() {
         
     }
     
-    int next(int price) 
-    {
+    int next(int price) {
         int span = 1;
         while(st.size()>0 && st.top().first <= price)
         {
@@ -81,12 +14,8 @@ public:
         }
         st.push({price, span});
         return span;
-    
     }
 };
-
-
-
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
