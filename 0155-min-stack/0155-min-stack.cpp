99d2f33b53fc390490ac0d7e822b9d3a4faaab0f
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-10-11 01:11:23 +0530

    Time: 20 ms (53.73%), Space: 16.6 MB (40.51%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,39 +1,37 @@
 class MinStack {
 public:
-    //Using extra space
-    
-    stack<int> s1, s2;
+    stack<int> st1, st2;
     MinStack() {
         
     }
     
     void push(int val) {
-        s1.push(val);
-        if(s2.size() == 0 || s2.top() >= val)  //agar s2 empty hai ya koi choti val aa rhi
-            s2.push(val);                       //hai tabhi push
+        st1.push(val);
+        if(st2.size() == 0 || st2.top() >= val)
+        {
+            st2.push(val);
+        }
         return;
     }
     
     void pop() {
-        if(s1.size() == 0)
+        if(st1.size() == 0)
             return;
-        int ans = s1.top();  
-        s1.pop();
-        if(s2.top() >= ans)  //agar min ele hi s1 se pop kr rhe hai to s2 se bhi
-            s2.pop();          //else agar s1 se pop hone vala s2 top i.e. min se bda
-                                //tab nhi pop
+        int ans = st1.top();
+        st1.pop();
+        if(st2.top() >= ans)
+            st2.pop();
     }
     
     int top() {
-        return s1.top();
+        return st1.top();
         
     }
     
     int getMin() {
-        if(s2.size() == 0)  //s2 ka top hme min de dega bcz hmne push hi vaise kiya hai
+        if(st2.size() == 0)
             return -1;
-        return s2.top();
-        
+        return st2.top();
     }
 };
 
