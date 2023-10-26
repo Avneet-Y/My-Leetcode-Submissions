dec5faffe4495ee1140961bcfd4dcd455911edbc
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-10-27 00:57:22 +0530

    Time: 168 ms (75.81%), Space: 63.5 MB (78.52%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> ans;
         int n = points.size();
-        priority_queue< pair<int, pair<int, int>> > maxh;
+        vector<vector<int>> ans;
+        priority_queue<pair<int, pair<int,int>>> maxh;
         for(int i=0; i<n; i++)
         {
-            maxh.push({points[i][0]*points[i][0] + points[i][1]*points[i][1], 
-                       {points[i][0], points[i][1]}});
-            if(maxh.size() > k)
+            int dis = points[i][1]*points[i][1] + points[i][0]*points[i][0];
+            maxh.push({dis, {points[i][0], points[i][1]}});
+            while(maxh.size() > k)
+            {
                 maxh.pop();
+            }
         }
-        while(!maxh.empty())
+        while(maxh.size() > 0)
         {
-            pair<int, int> p = maxh.top().second;
-            ans.push_back({p.first, p.second});
+            ans.push_back({maxh.top().second.first, maxh.top().second.second});
             maxh.pop();
         }
         return ans;
