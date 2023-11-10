43756eb06c847ca22ae3757b9cd4ee3433fa5d4d
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-11-11 01:08:18 +0530

    Time: 10 ms (18.29%), Space: 13.9 MB (58.80%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,61 +1,58 @@
 class Solution {
 public:
-    int binarylast(vector<int>& nums, int target, int n)
+    int firstocc(vector<int>& nums, int target, int n)
     {
         int start = 0;
         int end = n-1;
-        int last = -1;
-        
+        int res = -1;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
             if(nums[mid] == target)
             {
-                last = mid;
-                start = mid+1; 
+                res = mid;
+                end = mid-1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
             }
             else if(nums[mid] > target)
             {
                 end = mid-1;
             }
-            else
-            {
-                start = mid+1;
-            }
         }
-        return last;
+        return res;
     }
-    
-    int binaryfirst(vector<int>& nums, int target, int n)
+    int lastocc(vector<int>& nums, int target, int n)
     {
         int start = 0;
         int end = n-1;
-        int first = -1;
-        
+        int res = -1;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
             if(nums[mid] == target)
             {
-                first = mid;
-                end = mid-1; 
+                res = mid;
+                start = mid+1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
             }
             else if(nums[mid] > target)
             {
                 end = mid-1;
             }
-            else
-            {
-                start = mid+1;
-            }
         }
-        return first;
+        return res;
     }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int first = binaryfirst(nums, target, n);
-        int last = binarylast(nums, target, n);
+        int first = firstocc(nums, target, n);
+        int last = lastocc(nums, target, n);
         return {first, last};
+        
     }
 };
