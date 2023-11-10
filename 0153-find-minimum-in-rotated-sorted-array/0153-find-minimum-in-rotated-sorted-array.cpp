4403cefcf319a5115a0c4d01ba0a80272e874671
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2023-11-11 01:42:38 +0530

    Time: 3 ms (58.46%), Space: 10.4 MB (93.22%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int binary(vector<int>& nums, int n)
-    {
-        int start = 0, next, prev;
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int start = 0;
         int end = n-1;
+        int next, prev;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
             if(nums[start] <= nums[end])
                 return nums[start];
             next = (mid+1) % n;
-            prev = (mid + n - 1) % n;
-            if(nums[prev] >= nums[mid] && nums[next] >= nums[mid])
+            prev = (mid-1+n) % n;
+            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next])
+            {
                 return nums[mid];
-            else if(nums[start] <= nums[mid])
+            }
+            else if(nums[mid] >= nums[start])
+            {
                 start = mid+1;
+            }
             else if(nums[mid] <= nums[end])
+            {
                 end = mid-1;
+            }
         }
         return nums[start];
-    }
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int ans = binary(nums, n);
-        return ans;
-        
     }
 };
