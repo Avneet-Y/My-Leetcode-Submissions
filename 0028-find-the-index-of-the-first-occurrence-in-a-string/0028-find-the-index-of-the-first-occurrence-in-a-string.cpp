b47a4ac7fbd5cc86f2ebff0657ef98c999b624f1
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2024-01-07 00:25:36 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (29.10%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+//         int n1 = haystack.size();
+//         int n2 = needle.size();
+//         if(n1 < n2)
+//             return -1;
+        
+//         for(int i=0; i <= n1-n2; i++)
+//         {
+//             if(haystack.substr(i, n2) == needle)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+      
         int n1 = haystack.size();
         int n2 = needle.size();
-        if(n1 < n2)
-            return -1;
-        
-        for(int i=0; i <= n1-n2; i++)
+        for(int i=0; i<n1; i++)
         {
-            if(haystack.substr(i, n2) == needle)
+            int j = 0, ind = i;
+            while(j<n2 && haystack[ind] == needle[j])
             {
-                return i;
+                j++;
+                ind++;
             }
+            if(j == n2)
+                return i;
         }
         return -1;
-        
-        
-        
-        
-        
-        // int haysize = haystack.size();
-        // int needlesize = needle.size();
-        // int ans = -1;
-        // int ihay = 0, ineed = 0;
-        // int jhay = haysize-1, jneed = needlesize-1;
-        // while(ihay < haysize && jhay >= 0)
-        // {
-        //     if(haystack[ihay] != needle[ineed])
-        //     {
-        //         ihay++;
-        //     }
-        //     else if(haystack[ihay] == needle[ineed])
-        //     {
-        //         ans = ihay; 
-        //         ihay++;
-        //         ineed++;
-        //     }
-        // }
-        
     }
+    
 };
