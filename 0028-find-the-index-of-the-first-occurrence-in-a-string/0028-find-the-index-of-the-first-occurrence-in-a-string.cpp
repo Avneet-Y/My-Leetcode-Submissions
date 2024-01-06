05b0104351e1e30af7bfe101fe5319655d6618eb
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2024-01-07 00:18:11 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (29.10%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,15 +3,40 @@ public:
     int strStr(string haystack, string needle) {
         int n1 = haystack.size();
         int n2 = needle.size();
-        int ind = -1;
-        if(n2 > n1)
+        if(n1 < n2)
             return -1;
+        
         for(int i=0; i <= n1-n2; i++)
         {
-            string temp = haystack.substr(i, n2);
-            if(temp == needle)
-                return ind = i;
+            if(haystack.substr(i, n2) == needle)
+            {
+                return i;
+            }
         }
-        return ind;
+        return -1;
+        
+        
+        
+        
+        
+        // int haysize = haystack.size();
+        // int needlesize = needle.size();
+        // int ans = -1;
+        // int ihay = 0, ineed = 0;
+        // int jhay = haysize-1, jneed = needlesize-1;
+        // while(ihay < haysize && jhay >= 0)
+        // {
+        //     if(haystack[ihay] != needle[ineed])
+        //     {
+        //         ihay++;
+        //     }
+        //     else if(haystack[ihay] == needle[ineed])
+        //     {
+        //         ans = ihay; 
+        //         ihay++;
+        //         ineed++;
+        //     }
+        // }
+        
     }
 };
