cd40f4687a5f7fd528a4f68c8b34add23ca4f8ca
Avneet <98650591+Avneet-Y@users.noreply.github.com>
2024-01-06 23:59:52 +0530

    Time: 3 ms (74.35%), Space: 9.5 MB (91.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
         int n = strs.size();
-        sort(strs.begin(), strs.end());
-        string s0 = strs[0];
-        string s2 = strs[n-1];
-        int s = min(s0.size(), s2.size());
-        
-        for(int i=0; i<s; i++)
+        int s0 = strs[0].size();
+        for(int i=1; i<n; i++)
         {
-            if(s0[i] != s2[i])
+            for(int j=0; j<s0; j++)
             {
-                return ans;
+                if(strs[0][j] != strs[i][j])
+                {
+                    s0 = j;
+                    break;
+                }
             }
-            ans += s0[i];
         }
-        return ans;
+        return strs[0].substr(0, s0);
         
+//Another app
+//         string ans = "";
+//         int n = strs.size();
+//         sort(strs.begin(), strs.end());
+//         string s0 = strs[0];
+//         string s2 = strs[n-1];
+//         int s = min(s0.size(), s2.size());
+        
+//         for(int i=0; i<s; i++)
+//         {
+//             if(s0[i] != s2[i])
+//             {
+//                 return ans;
+//             }
+//             ans += s0[i];
+//         }
+//         return ans;
     }
 };
